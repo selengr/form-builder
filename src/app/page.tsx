6afdb1333fb288b835719eff8ba22849b9d5c91b
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-11-19 10:23:14 +0330

    remove

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <p className="text-xl text-fuchsia-600">سایا</p>
-    </main>
-  );
-}
--- src/providers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./MuiRtlProvider";
-export * from "./RootProvider";
-export * from "./ReactQueryClientProvider";
-export * from "./MuiThemeProvider";
-export * from "./NetworkProvider";

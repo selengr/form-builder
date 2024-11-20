4cc4cb6a97e55fff1618f20caf8482c706de2ef1
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-11-20 14:51:48 +0330

    🔧 add dependency and update ctailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/templates/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx,js,jsx,mdx}",
   ],
   theme: {
     extend: {},

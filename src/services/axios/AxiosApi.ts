aeb350052a4d5e984eb8918c24ec2494fa206cf8
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-11-20 14:53:03 +0330

    🔧 add axios api config

---
+++ src/services/axios/AxiosApi.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const AxiosApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/psya",
+});
+
+AxiosApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const { response } = error;
+
+    if (response && !response.ok) {
+      const { status } = response;
+      const errorText = (await response.data) || response.statusText;
+
+      let errorMessage = `API request error: ${status}`;
+      if (status === 401) {
+        errorMessage = "Authentication error occurred";
+      } else if (status === 403) {
+        errorMessage = "Authorization error occurred";
+      } else if (status === 400) {
+        errorMessage = "Bad Request";
+      } else if (status === 409) {
+        errorMessage = "Conflict";
+      }
+
+      console.error(errorMessage);
+      throw new Error(errorText);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default AxiosApi;

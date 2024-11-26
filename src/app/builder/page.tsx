fb607484c267f4af6d10f14e3a9532815bbda6d1
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-11-26 10:38:45 +0330

    🍎 add base url and remove redundency

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,7 +1,3 @@
 export default function FormBuilderPage() {
-  return (
-    <div className="w-[250px] flex justify-center h-[100px] bg-green-600">
-      فرم ساز
-    </div>
-  );
+  return <></>;
 }

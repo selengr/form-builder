a1a5fd848f0aeea8a0f462575620243182d25585
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-11-27 09:38:08 +0330

    💄 updated main panel in both mobile and desktop to be responsive

--- src/templates/layout/MainPanel.tsx
+++ src/templates/layout/MainPanel.tsx
@@ -1,61 +1,35 @@
-import Image from "next/image";
-import MresalatLogo from "@/../public/images/home-page/mresalat_logo.svg";
-import ProfileLogo from "@/../public/images/home-page/profile.webp";
-import LogoutIcon from "@/../public/images/home-page/logout.svg";
-import InfoIcon from "@/../public/images/home-page/info-icon.svg";
-import HeaderDesktop from "@/../public/images/home-page/new-mresalt-header.svg";
-import MenuSidebar from "@/components/SideBar/MenuSidebar";
+import FooterTab from "@/components/FooterTab/Footer";
+import MainSidebar from "@/components/MainSidebar/MainSidebar";
+import TopAppBar from "@/components/TopAppBar/TopAppBar";
+import { useResponsive } from "@/hooks/useResponsive";
+import { usePathname } from "next/navigation";
 
 export default function MainPanel({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isMobile = useResponsive("down", "md");
+  const path = usePathname();
+
   return (
     <div className="flex min-h-screen w-full gap-4">
-      <div className="w-24 min-h-screen bg-white">
-        <div className="flex flex-col gap-4 p-4">
-          <Image
-            src={MresalatLogo}
-            alt="Mresalat_Logo"
-            width={64}
-            height={15}
-          />
-          <Image
-            src={ProfileLogo}
-            alt="Profile_Logo"
-            width={64}
-            height={64}
-            className="rounded-full border-[2px] border-[#1758BA]"
-          />
+      {isMobile && path === "/" ? (
+        <div className="w-full flex flex-col">
+          <TopAppBar title="" />
+          <div className="w-full">{children}</div>
+          <FooterTab />
         </div>
-        <div className="h-[520px] w-[80px] bg-cover bg-[url('/images/home-page/right_sidebar_bg.svg')]">
-          <MenuSidebar />
+      ) : isMobile ? (
+        <div className="w-full flex flex-col">
+          <div className="w-full">{children}</div>
         </div>
-        <div className="flex flex-col gap-6 items-center justify-center p-4">
-          <button>
-            <Image src={InfoIcon} alt="Logout" width={24} height={24} />
-          </button>
-          <button className="flex item-center justify-center flex-col gap-1">
-            <Image src={LogoutIcon} alt="Logout" width={24} height={24} />
-            <span className="text-[10px] text-black font-bold">خروج</span>
-          </button>
-        </div>
-      </div>
-      <div className="w-[386px] min-h-screen bg-[#F7F7FF] px-5">
-        <div className="mt-4 w-full">
-          <Image
-            src={HeaderDesktop}
-            alt="header"
-            width={100}
-            height={82}
-            className="w-full"
-          />
-        </div>
-        <div className="flex justify-between mt-2 gap-2"></div>
-        <div className="w-full flex"></div>
-      </div>
-      <div>{children}</div>
+      ) : (
+        <>
+          <MainSidebar />
+          <div className="w-full">{children}</div>
+        </>
+      )}
     </div>
   );
 }

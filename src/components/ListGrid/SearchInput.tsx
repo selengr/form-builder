2a1fada5301e0bcb8c529d0c27054fd5b68786cb
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-11-27 15:21:47 +0330

    🍬 add search input for list grid

---
+++ src/components/ListGrid/SearchInput.tsx
@@ -0,0 +1,81 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import { useState } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+
+export default function SearchInput() {
+  const [value, setValue] = useState("");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: any) {
+    setValue(term);
+  }
+
+  function handleClick() {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("query", value);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+
+  const handleKeyDown = (event: any) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleClick();
+    }
+  };
+
+  return (
+    <Paper
+      component="form"
+      sx={{
+        boxShadow: "unset",
+        border: "1px solid #C9C9C9 ",
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: "100%",
+        borderRadius: "13px",
+      }}
+    >
+      <InputBase
+        style={{}}
+        sx={{ ml: 1, flex: 1, textAlign: "end" }}
+        placeholder="جستجو"
+        inputProps={{ "aria-label": "جستجو" }}
+        defaultValue={searchParams.get("query")?.toString()}
+        onKeyDown={handleKeyDown}
+        onChange={(e) => {
+          e.preventDefault();
+          handleSearch(e.target.value);
+        }}
+      />
+      <IconButton
+        type="button"
+        sx={{ p: "8px" }}
+        aria-label="search"
+        onClick={handleClick}
+      >
+        <Image
+          src="./images/home-page/search.svg"
+          width={23}
+          height={23}
+          alt="Add"
+          style={{
+            cursor: "pointer",
+          }}
+        />
+      </IconButton>
+    </Paper>
+  );
+}

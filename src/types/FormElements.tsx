2bde6ffd09f5d5cc5570a6864c573a726090c127
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-12-01 10:35:42 +0330

    🌀 add builder and form element types

---
+++ src/types/FormElements.tsx
@@ -0,0 +1,84 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { IFormElementConstructor, IFormOptionList, tempObj } from "./bulider";
+import { SpectralFormElement } from "../components/Fields/SpectralField";
+import { TextFieldFormElement } from "../components/Fields/TextField";
+import { TitleFieldFinishFormElement } from "../components/Fields/TitleFieldFinish";
+import { TitleFieldStartFormElement } from "../components/Fields/TitleFieldStart";
+import { MultipleChoiceFormElement } from "../components/Fields/MultipleChoiceField";
+import { MultipleChoiceImageFormElement } from "../components/Fields/MultipleChoiceImageField";
+
+export type ElementsType =
+  | "TEXT_FIELD"
+  | "MULTIPLE_CHOICE"
+  | "TitleFieldStart"
+  | "TitleFieldFinish"
+  | "MULTIPLE_CHOICE_IMAGE"
+  | "SPECTRAL";
+
+export type SubmitFunction = (key: number, value: string) => void;
+
+export type FormElement = {
+  questionType: ElementsType;
+
+  construct: ({
+    questionId,
+    questionGroupId,
+    formId,
+    title,
+    position,
+  }: IFormElementConstructor) => FormElementInstance;
+
+  designerBtnElement: {
+    label: string;
+    icon?: any;
+  };
+
+  designerComponent: React.FC<{
+    elementInstance: FormElementInstance;
+  }>;
+  formComponent: React.FC<{
+    elementInstance: FormElementInstance;
+    submitValue?: SubmitFunction;
+    isInvalid?: boolean;
+    defaultValue?: string;
+  }>;
+  propertiesComponent: React.FC<{
+    elementInstance: FormElementInstance;
+  }>;
+
+  validate: (formElement: FormElementInstance, currentValue: string) => boolean;
+};
+
+export type FormElementInstance = {
+  questionId: number;
+  questionGroupId?: number | null;
+  formId?: number;
+  title?: string;
+  questionType?: ElementsType;
+  position?: number | null;
+  questionPropertyList?: Record<string, any>;
+  optionList?: IFormOptionList[] | [] | null | undefined;
+  temp?: boolean | tempObj;
+  draft?: draftObj;
+  startPageMsg?: string;
+  description?: string;
+  spectralPlaceList?: [];
+};
+
+type draftObj = {
+  prevGroup: number;
+  position: number;
+};
+
+type FormElementsType = {
+  [key in ElementsType]: FormElement;
+};
+
+export const FormElements: FormElementsType = {
+  TEXT_FIELD: TextFieldFormElement,
+  MULTIPLE_CHOICE: MultipleChoiceFormElement,
+  SPECTRAL: SpectralFormElement,
+  TitleFieldStart: TitleFieldStartFormElement,
+  TitleFieldFinish: TitleFieldFinishFormElement,
+  MULTIPLE_CHOICE_IMAGE: MultipleChoiceImageFormElement,
+};
---
+++ src/types/bulider.ts
@@ -0,0 +1,124 @@
+export type IFormElementConstructor = {
+  questionId: number;
+  questionGroupId?: number | null;
+  formId?: number;
+  title?: string;
+  position?: number | null;
+  temp?: boolean | tempObj;
+  optionList?: IFormOptionList;
+  startPageMsg?: string;
+  description?: string;
+  endPageId?: number;
+};
+
+export type IEndPageList = {
+  endPageId: number;
+  formId: number;
+  description: string;
+};
+
+export type tempObj = {
+  prevPosition: number;
+  prevQuestionGroupId: number;
+};
+
+export type IFormOptionList = {
+  score: number;
+  position?: number;
+  isTarget?: boolean;
+  title: string;
+  link?: string;
+  id?: number | null;
+};
+
+export type ITextFieldFormPatternOptions = {
+  value: string;
+  label: string;
+}[];
+
+export type IQPLTextField = [
+  {
+    questionPropertyEnum: "TEXT_FIELD_PATTERN";
+    value: string;
+  },
+  {
+    questionPropertyEnum: "REQUIRED";
+    value: string;
+  },
+  {
+    questionPropertyEnum: "DESCRIPTION";
+    value: string | null;
+  },
+  {
+    questionPropertyEnum: "MINIMUM_LEN";
+    value: string | number | null;
+  },
+  {
+    questionPropertyEnum: "MAXIMUM_LEN";
+    value: string | number | null;
+  },
+];
+
+export type ISpectralQTapAndOptionsType = { value: string; label: string }[];
+
+export type IQPLSpectral = [
+  {
+    questionPropertyEnum: "SPECTRAL_TYPE";
+    value: string;
+  },
+  {
+    questionPropertyEnum: "REQUIRED";
+    value: string;
+  },
+  {
+    questionPropertyEnum: "DESCRIPTION";
+    value: string | null;
+  },
+  {
+    questionPropertyEnum: "SELECTION_TYPE";
+    value: string;
+  },
+  {
+    questionPropertyEnum: "STEP";
+    value: number | string;
+  },
+  {
+    questionPropertyEnum: "SPECTRAL_START";
+    value: number | string;
+  },
+  {
+    questionPropertyEnum: "SPECTRAL_END";
+    value: number | string;
+  },
+];
+
+export type IQPLMultipleChoice = [
+  {
+    questionPropertyEnum: "MULTI_SELECT";
+    value: string;
+  },
+  {
+    questionPropertyEnum: "REQUIRED";
+    value: string;
+  },
+  {
+    questionPropertyEnum: "RANDOMIZE_OPTIONS";
+    value: string;
+  },
+  {
+    questionPropertyEnum: "DESCRIPTION";
+    value: string | null;
+  },
+];
+
+export type ITest = {
+  name: string;
+};
+
+export type IChangeOrMovePositionApi = {
+  formBuilderId: number;
+  questionId: number;
+  questionGroupId: number;
+  targetQuestionGroupId: number | null;
+  newPosition: number;
+};

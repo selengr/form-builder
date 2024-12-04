bad34002f4da8a069739b6ae5cc9fe1df42a6467
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-12-04 16:39:26 +0330

    ✅ add finish  and start pages with their components

--- src/templates/builder/Designer.tsx
+++ src/templates/builder/Designer.tsx
@@ -2,8 +2,8 @@ import { memo } from "react";
 import CreateFieldDialog from "@/components/builder/CreateFieldDialog";
 import DesignerSidebar from "@/components/builder/DesignerSidebar";
 import KanbanBoard from "./kanban/KanbanBoard";
-// import DesignerStartPageElement from "./DesignerStartPageElement";
-// import DesignerFinishPageElement from "./DesignerFinishPageElement";
+import DesignerStartPageElement from "./kanban/DesignerStartPageElement";
+import DesignerFinishPageElement from "./kanban/DesignerFinishPageElement";
 
 const Designer = memo(function Designer() {
   return (
@@ -13,11 +13,11 @@ const Designer = memo(function Designer() {
         className="p-4 w-full min-h-full flex-grow max-w-[920px] rounded-lg flex overflow-y-auto flex-col items-center bg-white shadow-[0_0_2px_0_rgba(187,187,187,0.2),0_12px_24px_-4px_rgba(187,187,187,0.12)] gap-4"
         style={{ scrollbarWidth: "none", userSelect: "none" }}
       >
-        {/* <DesignerStartPageElement /> */}
+        <DesignerStartPageElement />
         <div className="w-full h-full flex flex-grow flex-1 items-center flex-col justify-start rounded-md gap-4">
           <KanbanBoard />
         </div>
-        {/* <DesignerFinishPageElement /> */}
+        <DesignerFinishPageElement />
       </div>
       <DesignerSidebar />
     </div>

c8d37db0bbce372ca032a4c66839c03253b4d559
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-12-06 13:58:31 +0330

    🍱 fix range slider oveflow issue

--- src/components/Slider/RangeSlider.tsx
+++ src/components/Slider/RangeSlider.tsx
@@ -3,14 +3,6 @@ import { Slider, styled } from "@mui/material";
 export const MyRangeSlider = styled(Slider)({
   color: "#2CDFC9",
   height: 30,
-  "&.MuiSlider-root": {
-    "& .MuiSlider-thumb": {
-      transform: "translate(1.5rem, -50%) !important",
-    },
-    "& .MuiSlider-thumb::after": {
-      transform: "translate(-32rem, -50%) !important",
-    },
-  },
   "& .MuiSlider-mark": {
     display: "none !important",
   },
@@ -19,18 +11,18 @@ export const MyRangeSlider = styled(Slider)({
     color: "gray",
   },
   "& .MuiSlider-markLabelActive": {
-    color: "#2CDFC9 !important",
+    color: "#1758BA !important",
     fontWeight: "600",
   },
   "& .MuiSlider-track": {
     marginLeft: "3px !important",
-    borderRadius: "50px",
+    borderRadius: "10px",
   },
   "& .MuiSlider-rail": {
     padding: "3px",
     backgroundColor: "white",
     outline: "1px solid #2CDFC9 !important",
-    borderRadius: "50px",
+    borderRadius: "10px",
   },
   "& .MuiSlider-thumb": {
     height: 40,
@@ -53,11 +45,14 @@ export const MyRangeSlider = styled(Slider)({
     height: 32,
     borderRadius: "50%",
     background: "#fff !important",
-    color: "#1758BA",
-    fontWeight: 700,
+    color: "gray",
     transform: "translate(0px, 13px)",
     "&::before": {
       display: "none",
+    },
+    "& span": {
+      fontSize: "15px",
+      color: "#1758BA",
     },
   },
   "& .MuiSlider-valueLabelOpen": {

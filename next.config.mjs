1edfeeb842176e688f607e3a775e418b91527814
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2024-12-06 13:56:00 +0330

    add dependencies and fix env links

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "172.16.11.24",
+        port: "8080",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;

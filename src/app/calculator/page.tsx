f7b5ae47dc660c319b531cc5a6b1e7780c753518
selengr <reza1997karbakhsh@gmail.com>
2024-12-07 22:47:44 +0330

    fire api questionList

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,13 +1,19 @@
 
+import AxiosApi from '@/services/axios/AxiosApi';
 import dynamic from 'next/dynamic'
  
 
 const AdvancedFormulaEditor = dynamic(() => import('@/components/calculator/AdvancedFormulaEditor'), { ssr: false })
  
-export default function Calculator() {
+export default async function Calculator() {
+  const res = await AxiosApi.get(`/question/q-and-c-custom-combo?customComboFilterModel=%7B%22type%22%3A%22COMBO%22%2C%22entity%22%3A%22QUESTIONS%22%2C%22mode%22%3A%22QUESTIONS_IN_FORM_BUILDER__ALL%22%2C%22input%22%3A%22%22%2C%22page%22%3A0%2C%22rows%22%3A10000%2C%22extMap%22%3A%7B%22formId%22%3A81%2C%22typeRequest%22%3A%22QAC_BY_FILTER%22%7D%7D`);
+ 
+  console.log('data====== :>> ', res.data);
+
+  
   return (
     <div  className="flex min-h-screen flex-col items-center justify-between p-24">
-        <AdvancedFormulaEditor />
+        <AdvancedFormulaEditor questionList={res.data} />
     </div>
   )
 }

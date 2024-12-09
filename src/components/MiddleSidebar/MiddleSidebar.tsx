1be6f0ebb52760b818d684f9e398899c6a69d7aa
selengr <reza1997karbakhsh@gmail.com>
2024-12-09 23:59:45 +0330

    MiddleSidebar

--- src/components/MiddleSidebar/MiddleSidebar.tsx
+++ src/components/MiddleSidebar/MiddleSidebar.tsx
@@ -7,16 +7,19 @@ export default function MiddleSidebar() {
 
   return (
     <div className="min-w-[400px] w-[400px] min-h-screen bg-[#F7F7FF] px-5 gap-8 flex flex-col">
-      <div className="mt-4 w-full">
+      <div className="my-5 w-full flex justify-center align-middle">
         <Image
-          src={HeaderDesktop}
+          src={"/images/logo/LOGO.svg"}
           alt="header"
-          width={100}
-          height={100}
-          className="w-full"
+          width={5}
+          height={5}
+          className="w-32"
         />
       </div>
-      <div className="w-full flex flex-col gap-4">
+      <div className="w-full flex flex-col gap-3 bg-[#F2F4F8] h-20 rounded-2xl">
+        <span>شرکت فرداپ</span>
+
+        {/*         
         <button
           className="h-[56px] bg-[#1758BA] shadow-none rounded-lg text-white text-[16px] font-bold"
           onClick={() => {
@@ -24,7 +27,7 @@ export default function MiddleSidebar() {
           }}
         >
           فرم ساز
-        </button>
+        </button> */}
       </div>
     </div>
   );

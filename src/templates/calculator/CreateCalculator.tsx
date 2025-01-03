6573d7ecb936c32e3ce2619fca2f60aaf1491040
selengr <reza1997karbakhsh@gmail.com>
2025-01-04 00:07:03 +0330

    update CreateCalculator

--- src/templates/calculator/CreateCalculator.tsx
+++ src/templates/calculator/CreateCalculator.tsx
@@ -1,38 +1,46 @@
-import { memo, useCallback, useState } from "react";
+import { useState } from "react";
 import LoadingButton from "@mui/lab/LoadingButton";
-import { SxProps, Theme } from "@mui/material";
+import { SxProps, Theme, Typography } from "@mui/material";
+import CreateCalculatorDialog from "./CreateCalculatorDialog";
 
 
 const buttonSx: SxProps<Theme> = {
-    height: 52,
-    width: "100%",
-    display: "flex",
-    color: "#6F6F6F",
-    cursor: "pointer",
-    marginTop: "10px",
-    alignItems: "center",
-    justifyContent: "center",
-    border: "1px dashed #DDE1E6",
-    "&:hover": {
-        backgroundColor: "#F7F7FF"
-    }
+height: 52,
+width: "100%",
+display: "flex",
+color: "#6F6F6F",
+cursor: "pointer",
+marginTop: "10px",
+alignItems: "center",
+justifyContent: "center",
+border: "1px dashed #DDE1E6",
+"&:hover": {
+backgroundColor: "#F7F7FF"
+}
 };
 
 
 const CreateCalculator = () => {
+const [open, setOpen] = useState<boolean>(false);
 
 
-    return (
-        <LoadingButton
-            variant="text"
-            // onClick={}
-            // loading={}
-            fullWidth
-            sx={buttonSx}
-        >
-            ایجاد محاسبه‌گر
-        </LoadingButton>
-    );
+
+return (
+<>
+<LoadingButton
+variant="text"
+onClick={() => setOpen(true)}
+// loading={}
+fullWidth
+sx={buttonSx}
+>
+ایجاد محاسبه‌گر
+</LoadingButton>
+
+<CreateCalculatorDialog open={open} setOpen={setOpen} />
+
+</>
+);
 };
 
 export default CreateCalculator;

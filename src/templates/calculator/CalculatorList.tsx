f451db63ef72e9705e8e830056482bb5046fb88c
selengr <reza1997karbakhsh@gmail.com>
2025-01-04 00:09:20 +0330

    update CalculatorList

--- src/templates/calculator/CalculatorList.tsx
+++ src/templates/calculator/CalculatorList.tsx
@@ -2,21 +2,24 @@
 
 import { ICalculator } from "@/types/calculator";
 import { CalculatorCard } from "./CalculatorCard";
+import CreateCalculator from "./CreateCalculator";
 
 interface ICalculatorListProps {
   calculators: ICalculator[] | any;
 }
 
 const CalculatorList: React.FC<ICalculatorListProps> = ({ calculators }) => {
-  console.log("calculators :>> ", calculators);
   return (
-    <div
-      dir="rtl"
-      className="bg-[#F7F7FF] rounded-lg p-[10px] flex flex-col justify-center items-center w-full gap-3"
-    >
-      {calculators?.map((calculator:any) => (
-        <CalculatorCard calculator={calculator} />
-      ))}
+    <div className="w-full max-w-md flex flex-col">
+      <div
+        dir="rtl"
+        className="bg-[#F7F7FF] rounded-lg p-[10px] w-full flex flex-col gap-3"
+      >
+        {calculators?.map((calculator: ICalculator) => (
+          <>{calculator.name && <CalculatorCard calculator={calculator} />}</>
+        ))}
+      </div>
+      <CreateCalculator />
     </div>
   );
 };

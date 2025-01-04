0725543a531b76e8be0c782149fc6608b6e16492
selengr <reza1997karbakhsh@gmail.com>
2025-01-04 23:03:41 +0330

    add menu to calculator list

--- src/templates/calculator/CalculatorCard.tsx
+++ src/templates/calculator/CalculatorCard.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ICalculatorCardProps } from "@/types/calculator";
 import EditCalculatorDialog from "./EditCalculatorDialog";
+import { PhDotsThreeVerticalBold } from "../../../public/images/icons/PhDotsThreeVerticalBold";
+import { Menu, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { SlPencil } from "react-icons/sl";
+import { WeuiDeleteOutlined } from "../../../public/images/icons/DeleteIcon";
 
 export function CalculatorCard({ calculator }: ICalculatorCardProps) {
-  const [open, setOpen] = useState<boolean>(false);
+  const [openDialog, setOpen] = useState<boolean>(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = useCallback((event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    // if (loading) return;
+    setAnchorEl(null);
+  }, []);
 
   return (
     <>
@@ -13,7 +30,6 @@ export function CalculatorCard({ calculator }: ICalculatorCardProps) {
         className="bg-white rounded-lg p-[10px] h-14
 flex justify-between w-full cursor-pointer border-[1px]
 border-[#1758BA]"
-        onClick={() => setOpen(true)}
       >
         <div className="flex justify-center items-center gap-[10px]">
           <div
@@ -44,13 +60,76 @@ justify-center items-center"
             className="bg-[#F7F7FF] h-8 w-8 rounded-[10px] flex
 justify-center items-center"
           >
-            1
+            <button onClick={handleClick}>
+              <PhDotsThreeVerticalBold color="#1758BA" fontSize="1.5rem" />
+            </button>
+
+            {open && (
+              <Menu
+                sx={{
+                  "& .MuiPaper-root.MuiPaper-elevation": {
+                    borderRadius: "15px",
+                  },
+                  "& .MuiPaper-root": {
+                    touchAction: "none",
+                    width: "125px",
+                  },
+                  "& .MuiLoadingButton-label": {
+                    width: "100%",
+                  },
+                }}
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <LoadingButton
+                  sx={{
+                    paddingX: "10px",
+                    height: "36px",
+                    borderRadius: "10px",
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "row-reverse",
+                    justifyContent: "space-between",
+                    color: "#1758BA",
+                  }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setOpen(true);
+                    handleClose();
+                  }}
+                >
+                  <Typography>ویرایش</Typography>
+                  <SlPencil size="1.18rem" />
+                </LoadingButton>
+                <LoadingButton
+                  sx={{
+                    paddingX: "10px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    color: "#FA4D56",
+                  }}
+                  onClick={async (e) => {
+                    e.stopPropagation();
+                  }}
+                  fullWidth
+                  disabled={true}
+                >
+                  <Typography>حذف</Typography>
+                  <WeuiDeleteOutlined fontSize="1.32rem" />
+                </LoadingButton>
+              </Menu>
+            )}
           </div>
         </div>
       </div>
-      {open && (
+      {openDialog && (
         <EditCalculatorDialog
-          open={open}
+          open={openDialog}
           setOpen={setOpen}
           calcId={calculator.id}
         />

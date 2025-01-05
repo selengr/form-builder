b3fec0ece7ac22c00e92decbe3b09e17a827b8aa
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2025-01-05 16:59:52 +0330

    customize swtich button

--- src/components/Switch/SwitchButton.tsx
+++ src/components/Switch/SwitchButton.tsx
@@ -10,38 +10,52 @@ export const SwitchButton = styled(Switch)(({ theme }) => ({
     margin: 2,
     transitionDuration: "300ms",
     "&.Mui-checked": {
-      transform: "translateX(19px)",
+      transform: "translateX(23px)",
       color: "white",
       "& + .MuiSwitch-track": {
-        backgroundColor: "#1758BA",
+        backgroundColor: "white",
+        border: "1px solid #006FFF",
         opacity: 1,
       },
       "&.Mui-disabled + .MuiSwitch-track": {
         opacity: 0.5,
+        border: `1px solid ${theme.palette.grey[300]} !important`,
       },
       "& .MuiSwitch-thumb": {
-        border: `none !important`,
+        border: `1px solid white !important`,
       },
     },
     "&.Mui-focusVisible .MuiSwitch-thumb": {
       color: "white",
     },
     "&.Mui-disabled .MuiSwitch-thumb": {
-      color: theme.palette.grey[100],
+      color: theme.palette.grey[300],
     },
     "&.Mui-disabled + .MuiSwitch-track": {
       opacity: 0.3,
     },
   },
+  "& .Mui-checked .MuiSwitch-thumb": {
+    boxSizing: "border-box",
+    width: 15,
+    height: 15,
+    boxShadow: "none",
+    color: "#006FFF",
+    marginTop: "2px",
+  },
   "& .MuiSwitch-thumb": {
     boxSizing: "border-box",
-    width: 19,
-    height: 19,
+    width: 15,
+    height: 15,
     boxShadow: "none",
+    color: theme.palette.grey[500],
+    border: `1px solid white !important`,
+    marginTop: "2px",
   },
   "& .MuiSwitch-track": {
     borderRadius: 26 / 2,
-    backgroundColor: theme.palette.grey[300],
+    backgroundColor: "white",
+    border: `1px solid ${theme.palette.grey[400]}`,
     opacity: 1,
     transition: theme.transitions.create(["background-color"], {
       duration: 500,

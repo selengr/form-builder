85d908465fdbc1e07385f45fe656b24cfc64036d
selengr <reza1997karbakhsh@gmail.com>
2025-02-05 00:05:08 +0330

    custom hook for useGetQacWithOutFilter

--- src/app/(builder)/builder/[id]/condition/_hooks/useGetQacWithOutFilter.ts
+++ src/app/(builder)/builder/[id]/condition/_hooks/useGetQacWithOutFilter.ts
@@ -1,14 +1,13 @@
 
+import AxiosApi from '@/services/axios/AxiosApi';
 import { useQuery } from '@tanstack/react-query';
 
-const fetchData = async () => {
-  const response = await fetch('http://localhost:3000/api/question/q-and-c-custom-combo?customComboFilterModel=%7B%22type%22%3A%22COMBO%22%2C%22entity%22%3A%22QUESTIONS%22%2C%22mode%22%3A%22QUESTIONS_IN_FORM_BUILDER__ALL%22%2C%22input%22%3A%22%22%2C%22page%22%3A0%2C%22rows%22%3A10000%2C%22extMap%22%3A%7B%22formId%22%3A81%2C%22typeRequest%22%3A%22QAC_WIHT_OUT_FILTER%22%7D%7D');
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
-};
 
+const fetchData = async () => {
+    const url = `question/q-and-c-custom-combo?customComboFilterModel=%7B%22type%22%3A%22COMBO%22%2C%22entity%22%3A%22QUESTIONS%22%2C%22mode%22%3A%22QUESTIONS_IN_FORM_BUILDER__ALL%22%2C%22input%22%3A%22%22%2C%22page%22%3A0%2C%22rows%22%3A10000%2C%22extMap%22%3A%7B%22formId%22%3A81%2C%22typeRequest%22%3A%22QAC_WIHT_OUT_FILTER%22%7D%7D`;
+    const response = await AxiosApi.get(url);
+    return response.data;
+  };
 
 
 export const useGetQacWithOutFilter = () => {

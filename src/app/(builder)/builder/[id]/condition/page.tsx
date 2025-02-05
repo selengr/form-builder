50bb943d8c5ec625edf710a386a3de9bf8b8fbb5
selengr <reza1997karbakhsh@gmail.com>
2025-02-05 20:47:25 +0330

    change condition page

--- src/app/(builder)/builder/[id]/condition/page.tsx
+++ src/app/(builder)/builder/[id]/condition/page.tsx
@@ -1,11 +1,21 @@
-import { ConditionalSystem } from "@/templates/condition/ConditionalSystem";
 
 
-export default function Page({params}:{params : {id:string} }) {
-    return (
-        <>
-            <ConditionalSystem id={parseInt(params.id)}/>
-        </>
-    );
+import dynamic from 'next/dynamic'
+import { AxiosResponse } from 'axios';
+import AxiosApi from '@/services/axios/AxiosApi';
+
+
+const ConditionList = dynamic(() => import('@/templates/condition/ConditionList'), { ssr: false })
+ 
+export default async function Calculator({params}:{params : {id:string} }) {
+ const url = `/calculation/main-list/${params.id}?searchFilterModel=%7B%22searchFilterBoxList%22%3A%5B%7B%22restrictionList%22%3A%5B%5D%7D%5D%2C%22sortList%22%3A%5B%7B%22fieldName%22%3A%22id%22%2C%22type%22%3A%22DSC%22%7D%5D%2C%22page%22%3A0%2C%22rows%22%3A1000%7D`;
+ const conditions : AxiosResponse<any> = await AxiosApi.get(url);
+ const {data : {content}} = conditions
+
+ 
+  return (
+    <div className="container mx-auto py-8 flex justify-center items-start h-screen bg-white">
+        <ConditionList conditions={content}/>
+    </div>
+  )
 }
-

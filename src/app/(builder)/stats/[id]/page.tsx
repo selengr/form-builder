ccee15e2138e51088b760665a3f7b432e1da2ecd
amirhosseinhesami <amirhosseinhesami@fardup.ir>
2025-03-17 15:35:07 +0330

    End Game 💥

--- src/app/(builder)/stats/[id]/page.tsx
+++ src/app/(builder)/stats/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Box,
   Button,
@@ -14,6 +14,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
@@ -22,6 +23,9 @@ import { IoIosArrowForward } from "react-icons/io";
 import { IoStatsChartOutline } from "react-icons/io5";
 import { LuUserRoundPlus } from "react-icons/lu";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import AxiosApi from "@/services/axios/AxiosApi";
+import { useParams } from "next/navigation";
+import { ImSpinner2 } from "react-icons/im";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -81,8 +85,33 @@ const rows = [
 ];
 
 export default function StatsPage() {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [headData, setHeadData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [allData, setAllData] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const res = await AxiosApi.get(
+          `/report/solo/answers-data-sheet/${id}?searchFilterModel=%7B%22searchFilterBoxList%22%3A%5B%7B%22restrictionList%22%3A%5B%5D%7D%5D%2C%22sortList%22%3A%5B%7B%22fieldName%22%3A%22id%22%2C%22type%22%3A%22DSC%22%7D%5D%2C%22page%22%3A0%2C%22rows%22%3A100000%7D`
+        );
+
+        setIsLoading(false);
+
+        setHeadData([
+          ...res.data.content[0].row,
+          { questionId: Math.random(), questionTitle: "عملیات" },
+        ] as any);
+        setAllData(res.data.content);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <Box bgcolor="#fff" width="100%" padding={2}>
@@ -127,130 +156,190 @@ export default function StatsPage() {
         </Box>
         <Box></Box>
       </Box>
-      <Box sx={{ width: "100%", paddingX: 2, paddingBottom: 2 }}>
+      <Box
+        sx={{
+          width: "100%",
+          paddingX: 2,
+          paddingBottom: 2,
+        }}
+      >
         <TableContainer
           component={Paper}
           sx={{
             width: "100%",
+            height: "calc(100vh - 150px)",
             "&.MuiPaper-root": {
               boxShadow: "none",
               borderRadius: "12px",
             },
           }}
         >
-          <Table sx={{ minWidth: 700 }}>
-            <TableHead
-              sx={{
-                borderBottom: "5px solid white",
-              }}
-            >
-              <TableRow>
-                {tableHead.map((item: any) => (
-                  <StyledTableCell key={item.id}>{item.title}</StyledTableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <StyledTableRow key={row.id}>
-                  <StyledTableCell>{row.id}</StyledTableCell>
-                  <StyledTableCell>{row.fullName}</StyledTableCell>
-                  <StyledTableCell align="right">{row.q1}</StyledTableCell>
-                  <StyledTableCell align="right">{row.q2}</StyledTableCell>
-                  <StyledTableCell align="right">{row.q3}</StyledTableCell>
-                  <StyledTableCell align="right">{row.q4}</StyledTableCell>
-                  <StyledTableCell align="right">
-                    <Box
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="flex-end"
-                      gap={1}
-                    >
-                      <IconButton
+          {isLoading ? (
+            <div className="w-full h-full flex justify-center items-center">
+              <ImSpinner2 className="animate-spin h-12 w-12" />
+            </div>
+          ) : (
+            <>
+              <Table sx={{ minWidth: 700 }}>
+                <TableHead
+                  sx={{
+                    borderBottom: "5px solid white",
+                  }}
+                >
+                  <TableRow>
+                    {headData.map((item: any) => (
+                      <StyledTableCell
                         sx={{
-                          borderRadius: "8px",
-                          bgcolor: row.id === 3 ? "#FA4D56" : "#2CDFC9",
-                          "&:hover": {
-                            bgcolor: row.id === 3 ? "#FA4D56" : "#2CDFC9",
+                          "&.MuiTableCell-root": {
+                            minWidth: "200px",
+                            width: "200px",
+                            maxWidth: "200px",
+                            textOverflow: "ellipsis",
+                            overflow: "hidden",
+                            textWrap: "nowrap",
                           },
                         }}
+                        key={item.questionId}
                       >
-                        <LuUserRoundPlus color="#fff" />
-                      </IconButton>
-                      <IconButton
-                        sx={{
-                          borderRadius: "8px",
-                          bgcolor: "#1758BA",
-                          "&:hover": {
-                            bgcolor: "#1758BA",
-                          },
-                        }}
-                      >
-                        <IoStatsChartOutline color="#fff" />
-                      </IconButton>
-                    </Box>
-                  </StyledTableCell>
-                </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <Box
-            sx={{
-              bgcolor: "#f7f7f7",
-              width: "100%",
-              display: "flex",
-              justifyContent: "space-between",
-              paddingX: 2,
-              paddingY: 1,
-              gap: 1,
-              flexWrap: "wrap",
-            }}
-          >
-            <Box display="flex" gap={1} alignItems="center">
-              <Typography>سطر قابل نمایش در هر صفحه:</Typography>
-              <Select
+                        <Tooltip
+                          disableTouchListener
+                          enterDelay={1000}
+                          leaveDelay={100}
+                          title={item.questionTitle}
+                          arrow
+                        >
+                          <p
+                            dir="rtl"
+                            className="text-base overflow-hidden text-ellipsis w-full"
+                            style={{ textWrap: "nowrap", fontWeight: "700" }}
+                          >
+                            {item.questionTitle}
+                          </p>
+                        </Tooltip>
+                      </StyledTableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {allData.map((row: any) => (
+                    <StyledTableRow key={row.row.questionId}>
+                      {row.row.map((data: any, index: number) => (
+                        <Tooltip key={index} title={data.answer.join(" - ")}>
+                          <StyledTableCell
+                            sx={{
+                              "&.MuiTableCell-root": {
+                                minWidth: "200px",
+                                width: "200px",
+                                maxWidth: "200px",
+                                textOverflow: "ellipsis",
+                                overflow: "hidden",
+                                textWrap: "nowrap",
+                              },
+                            }}
+                          >
+                            {data.answer.map((d: string, index: number) => (
+                              <span key={index}>
+                                {d}
+                                {index < data.answer.length - 1 && " - "}
+                              </span>
+                            ))}
+                          </StyledTableCell>
+                        </Tooltip>
+                      ))}
+                      <StyledTableCell align="right">
+                        <Box
+                          display="flex"
+                          alignItems="center"
+                          justifyContent="flex-end"
+                          gap={1}
+                        >
+                          <IconButton
+                            sx={{
+                              borderRadius: "8px",
+                              bgcolor: row.id === 3 ? "#FA4D56" : "#2CDFC9",
+                              "&:hover": {
+                                bgcolor: row.id === 3 ? "#FA4D56" : "#2CDFC9",
+                              },
+                            }}
+                          >
+                            <LuUserRoundPlus color="#fff" />
+                          </IconButton>
+                          <IconButton
+                            sx={{
+                              borderRadius: "8px",
+                              bgcolor: "#1758BA",
+                              "&:hover": {
+                                bgcolor: "#1758BA",
+                              },
+                            }}
+                          >
+                            <IoStatsChartOutline color="#fff" />
+                          </IconButton>
+                        </Box>
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              <Box
                 sx={{
-                  "&.MuiInputBase-root": {
-                    bgcolor: "white",
-                    borderRadius: "12px",
-                    height: "36px",
-                  },
+                  bgcolor: "#f7f7f7",
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  paddingX: 2,
+                  paddingY: 1,
+                  gap: 1,
+                  flexWrap: "wrap",
                 }}
               >
-                <MenuItem value={5}>5</MenuItem>
-                <MenuItem value={10}>10</MenuItem>
-                <MenuItem value={25}>25</MenuItem>
-                <MenuItem value={"all"}>همه</MenuItem>
-              </Select>
-            </Box>
-            <Box display="flex" gap={1} alignItems="center">
-              <IconButton
-                sx={{
-                  bgcolor: "white",
-                  border: "1px solid #1758BA",
-                  borderRadius: "50%",
-                }}
-              >
-                <MdKeyboardArrowRight size="1.5rem" color="#1758BA" />
-              </IconButton>
-              <Typography>صفحه 1 از 3</Typography>
-              <IconButton
-                sx={{
-                  bgcolor: "white",
-                  border: "1px solid #1758BA",
-                  borderRadius: "50%",
-                }}
-              >
-                <MdKeyboardArrowLeft size="1.5rem" color="#1758BA" />
-              </IconButton>
-            </Box>
-            <Box display="flex" gap={1} alignItems="center">
-              <Typography>30 نفر در لیست</Typography>
-              <Box bgcolor="#1758BA" padding={1} borderRadius="12px">
-                <LuUserRoundPlus size="1.5rem" color="white" />
+                <Box display="flex" gap={1} alignItems="center">
+                  <Typography>سطر قابل نمایش در هر صفحه:</Typography>
+                  <Select
+                    sx={{
+                      "&.MuiInputBase-root": {
+                        bgcolor: "white",
+                        borderRadius: "12px",
+                        height: "36px",
+                      },
+                    }}
+                  >
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={10}>10</MenuItem>
+                    <MenuItem value={25}>25</MenuItem>
+                    <MenuItem value={"all"}>همه</MenuItem>
+                  </Select>
+                </Box>
+                <Box display="flex" gap={1} alignItems="center">
+                  <IconButton
+                    sx={{
+                      bgcolor: "white",
+                      border: "1px solid #1758BA",
+                      borderRadius: "50%",
+                    }}
+                  >
+                    <MdKeyboardArrowRight size="1.5rem" color="#1758BA" />
+                  </IconButton>
+                  <Typography>صفحه 1 از 1</Typography>
+                  <IconButton
+                    sx={{
+                      bgcolor: "white",
+                      border: "1px solid #1758BA",
+                      borderRadius: "50%",
+                    }}
+                  >
+                    <MdKeyboardArrowLeft size="1.5rem" color="#1758BA" />
+                  </IconButton>
+                </Box>
+                <Box display="flex" gap={1} alignItems="center">
+                  <Typography>{allData.length} نفر در لیست</Typography>
+                  <Box bgcolor="#1758BA" padding={1} borderRadius="12px">
+                    <LuUserRoundPlus size="1.5rem" color="white" />
+                  </Box>
+                </Box>
               </Box>
-            </Box>
-          </Box>
+            </>
+          )}
         </TableContainer>
       </Box>
     </Box>

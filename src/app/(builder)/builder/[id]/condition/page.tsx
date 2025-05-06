c69ebff356a7f809efe6df71969efe74c769a839
mjsoleimani <mjavadsf72@gmail.com>
2025-05-06 09:14:57 +0330

    refactor(condition-page): migrate to client component
    Migrate the condition page to a client component to utilize hooks.
    - Use `useListFetcher` hook for data fetching.
    - Improve performance by fetching data on the client side.
    
    feat(useListFetcher): implement condition data fetch hook
    
    Implement a custom hook to manage condition data fetching.
    - Decouple component logic from API requests.
    - Provide loading state for better UX.

--- src/app/(builder)/builder/[id]/condition/page.tsx
+++ src/app/(builder)/builder/[id]/condition/page.tsx
@@ -1,10 +1,8 @@
-import AxiosApi from '@/services/axios/AxiosApi'
-import ClientView from './ClientView'
+"use client";
+import {useListFetcher} from "@/hooks/useListFetcher";
+import ClientView from "./ClientView";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const url = `/condition/main-list/${params.id}?searchFilterModel=%7B%22searchFilterBoxList%22%3A%5B%7B%22restrictionList%22%3A%5B%5D%7D%5D%2C%22sortList%22%3A%5B%7B%22fieldName%22%3A%22id%22%2C%22type%22%3A%22DSC%22%7D%5D%2C%22page%22%3A0%2C%22rows%22%3A1000%7D`;
-  const res = await AxiosApi.get(url)
-  const { data: { content } } = res
-
-  return <ClientView conditions={content} />
+export default function ConditionPage({params}: { params: { id: string } }) {
+  const {data: conditions, loading} = useListFetcher({path: "condition", id: params.id});
+  return <ClientView conditions={conditions}/>;
 }

7990b786e7fec6691ea539371b425ba8487ff02f
mjsoleimani <mjavadsf72@gmail.com>
2025-05-24 11:37:45 +0330

    feat(top-appbar): enhance user auth and profile link
    - Refactor login/logout to single auth button.
    - Link user avatar to profile page.
    - Add conditional rendering for login/profile.
    
    This change improves user experience by streamlining authentication and providing a direct link to the user profile.

--- src/components/TopAppBar/TopAppBar.tsx
+++ src/components/TopAppBar/TopAppBar.tsx
@@ -6,28 +6,28 @@ import Link from "next/link";
 import {useRouter} from "next/navigation";
 import {signIn, signOut} from "next-auth/react";
 import {toast} from "sonner";
-
 import Avatar from "../Avatar/Avatar";
 import {useUserInfo} from "@/hooks/useUserInfo";
 
 const TopAppBar = ({customActions, appBarSx, toolbarSx, imageSx}: any) => {
   const router = useRouter();
-  const {userInfo, loading, error} = useUserInfo();
+  const {userInfo, loading} = useUserInfo();
+  const endPoint = process.env.NEXT_PUBLIC_MRESALAT_ENDPOINT || "";
 
-  const handleLogin = async () => {
-    await signIn("authorize");
+  const handleAuth = async () => {
+    if (userInfo) {
+      await signOut({callbackUrl: "/"});
+      toast.success("خروج با موفقیت انجام شد");
+    } else {
+      await signIn("authorize");
+    }
   };
 
-  const handleLogout = async () => {
-    toast.success("خروج با موفقیت انجام شد");
-    await signOut({callbackUrl: "/"});
-  };
-
-  const renderUserSection = () => {
+  const renderUser = () => {
     if (loading) {
-      return (<div className="flex items-center">
+      return (<div className="flex items-center gap-4">
           <Skeleton variant="circular" width={50} height={50}/>
-          <div className="ml-4">
+          <div>
             <Skeleton width={100}/>
             <Skeleton width={80}/>
           </div>
@@ -35,81 +35,51 @@ const TopAppBar = ({customActions, appBarSx, toolbarSx, imageSx}: any) => {
     }
 
     if (userInfo) {
-      return (<Link href="">
-          <div className="gap-[5px] flex items-center">
+      return (<Link href={`${endPoint}/profile`}>
+          <div className="flex items-center gap-2 cursor-pointer">
             <Avatar size="sm" name={userInfo?.user?.fullName || "کاربر"}/>
             <div>
-              <Typography variant="body1" color="black">
-                {userInfo?.user?.fullName}
-              </Typography>
-              <Typography variant="caption" color="black">
-                مشاهده پروفایل
-              </Typography>
+              <Typography variant="body1">{userInfo.user.fullName}</Typography>
+              <Typography variant="caption">مشاهده پروفایل</Typography>
             </div>
           </div>
         </Link>);
     }
 
-    return (<div onClick={handleLogin}>
-        <IconButton
-          size="medium"
-          disableRipple
-          sx={{display: "flex", gap: "8px"}}
-        >
-          <Image
-            src="/images/home-page/login.svg"
-            alt="ورود"
-            width={24}
-            height={24}
-          />
-          {customActions ?? <Typography color="#424242">ورود</Typography>}
-        </IconButton>
-      </div>);
+    return (<IconButton onClick={handleAuth} sx={{gap: 1}}>
+        <Image src="/images/home-page/login.svg" alt="ورود" width={24} height={24}/>
+        {customActions ?? <Typography color="#424242">ورود</Typography>}
+      </IconButton>);
   };
 
   return (<AppBar
       elevation={0}
       position="static"
-      sx={{
-        pt: 2, height: "100px", color: "black", backgroundColor: "#fff", ...appBarSx,
-      }}
+      sx={{pt: 2, height: "100px", bgcolor: "#fff", color: "black", ...appBarSx}}
     >
       <Toolbar sx={toolbarSx}>
-        <Box sx={{mr: 0, ...imageSx}}>{renderUserSection()}</Box>
+        <Box sx={{...imageSx}}>{renderUser()}</Box>
+        <Box sx={{flexGrow: 1}}/>
 
-        <div className="flex-grow"/>
-
-        <div className="flex items-center">
+        <Box className="flex items-center gap-2">
           <IconButton size="small" onClick={() => router.push("#")}>
-            <Image
-              src="/images/home-page/search.svg"
-              alt="search"
-              width={24}
-              height={24}
-            />
+            <Image src="/images/home-page/search.svg" alt="search" width={24} height={24}/>
           </IconButton>
 
           <IconButton size="small">
-            <Image
-              src="/images/home-page/notification.svg"
-              alt="notification"
-              width={24}
-              height={24}
-            />
+            <Image src="/images/home-page/notification.svg" alt="notification" width={24} height={24}/>
           </IconButton>
 
-          {!loading && userInfo && (<div onClick={handleLogout}>
-              <IconButton size="medium" disableRipple sx={{gap: "8px"}}>
-                <Image
-                  className="rotate-180"
-                  src="/images/home-page/login.svg"
-                  alt="خروج"
-                  width={24}
-                  height={24}
-                />
-              </IconButton>
-            </div>)}
-        </div>
+          {!loading && userInfo && (<IconButton onClick={handleAuth}>
+              <Image
+                className="rotate-180"
+                src="/images/home-page/login.svg"
+                alt="خروج"
+                width={24}
+                height={24}
+              />
+            </IconButton>)}
+        </Box>
       </Toolbar>
     </AppBar>);
 };

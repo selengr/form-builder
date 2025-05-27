177bdb5f9120b1cfe2cb335cc2833666e50e652b
selengr <reza1997karbakhsh@gmail.com>
2025-05-27 14:08:03 +0330

    simplify code

--- src/app/(builder)/builder/[id]/calculator/ClientView.tsx
+++ src/app/(builder)/builder/[id]/calculator/ClientView.tsx
@@ -1,27 +1,24 @@
 "use client";
 
 import dynamic from "next/dynamic";
+// _hooks
+import { useGetList } from "./_hooks/useGetList";
 import DesignerTabs from "@/templates/builder/TabComponent";
 import CalculatorSkeleton from "@/templates/calculator/CalculatorSkeleton";
 
 const CalculatorList = dynamic(
   () => import("@/templates/calculator/CalculatorList")
 );
-export default function ClientView({
-  calculators,
-  isPending,
-  error,
-}: {
-  calculators: any[];
-  isPending: boolean;
-  error: any;
-}) {
+
+export default function ClientView() {
+  const { data, isPending, error } = useGetList();
+
   return (
     <div className="w-full min-h-full px-4 py-4 ">
       <div className="relative container mx-auto flex flex-col justify-start items-center h-full bg-white rounded-xl w-full">
         <DesignerTabs />
         {!error && isPending && <CalculatorSkeleton />}
-        {!error && !isPending && <CalculatorList calculators={calculators} />}
+        {!error && !isPending && <CalculatorList calculators={data} />}
         {error && (
           <div className="flex flex-col absolute top-[250px] justify-center items-center">
             <span className="text-red-500">
--- src/app/(builder)/builder/[id]/calculator/page.tsx
+++ src/app/(builder)/builder/[id]/calculator/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import ClientView from "./ClientView";
-import { useGetList } from "./_hooks/useGetList";
 
 export default function CalculatorPage() {
-  const { data, isPending, error } = useGetList();
-  console.log('data error :>> ', error);
-  return <ClientView calculators={data} isPending={isPending} error={error} />;
+  return <ClientView />;
 }

693919b2797e92e90315d9a7850f8c3d19cf69d7
selengr <reza1997karbakhsh@gmail.com>
2025-05-27 19:07:01 +0330

    improving condition part three

--- src/app/(builder)/builder/[id]/condition/ClientView.tsx
+++ src/app/(builder)/builder/[id]/condition/ClientView.tsx
@@ -5,7 +5,12 @@ import DesignerTabs from '@/templates/builder/TabComponent'
 
 const ConditionList = dynamic(() => import('@/templates/condition/ConditionList'))
 
-export default function ClientView({conditions}: { conditions: any[] }) {
+interface IProps<T> {
+  conditions: any|T[];
+  isPending: boolean;
+  error: Error | null;
+}
+export default function ClientView<T>({ conditions, isPending, error }: IProps<T>) {
   return (
     <div className="w-full min-h-screen px-4 py-4">
       <div className="container mx-auto flex flex-col justify-start items-center bg-white rounded-xl w-full">

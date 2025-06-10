38e6650ea12179ce3ce567ece34a45b6066eba91
mjsoleimani <mjavadsf72@gmail.com>
2025-06-10 22:58:07 +0330

    refactor(multiple-choice): increase character limits for options and descriptions
    Increase the maximum character limits for multiple-choice options and descriptions to accommodate longer text. This change improves the flexibility and usability of the component, allowing users to provide more detailed answers and explanations.
    
    • Increased the maximum length for options to 500 characters.
    • Increased the maximum length for question titles to 4000 characters.
    • Increased the maximum length for descriptions to 4000 characters.
    
    ⏱️Time spent: 0h 15m

--- src/components/Fields/MultipleChoiceField.tsx
+++ src/components/Fields/MultipleChoiceField.tsx
@@ -96,8 +96,8 @@ const optionsSchema = z.object({
         .min(1, {
           message: "هر گزینه حداقل باید 1 و حداکثر 50 کاراکتر داشته باشد",
         })
-        .max(50, {
-          message: "هر گزینه حداقل باید 1 و حداکثر 50 کاراکتر داشته باشد",
+        .max(500, {
+          message: "هر گزینه حداقل باید 1 و حداکثر 500 کاراکتر داشته باشد",
         })
     ),
   score: z.number(),
@@ -111,15 +111,15 @@ const propertiesSchema = z.object({
     .pipe(
       z
         .string()
-        .min(1, { message: "حداقل باید 1 و حداکثر 100 کاراکتر باشد" })
-        .max(100, { message: "حداقل باید 1 و حداکثر 100 کاراکتر باشد" })
+        .min(1, { message: "حداقل باید 1 و حداکثر 4000 کاراکتر باشد" })
+        .max(3999, { message: "حداقل باید 1 و حداکثر 4000 کاراکتر باشد" })
     ),
   DESCRIPTION: z.object({
     value: z
       .string()
       .trim()
       .transform((value) => value.replace(/\s+/g, " "))
-      .pipe(z.string().max(250, { message: "حداکثر میتواند 250 کاراکتر باشد" }))
+      .pipe(z.string().max(3999, { message: "حداکثر میتواند 4000 کاراکتر باشد" }))
       .optional(),
     id: z.number(),
   }),

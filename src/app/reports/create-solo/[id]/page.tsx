cccd7e9d61e2d53c5aea8d026fd8cb7d64e4f89c
selengr <reza1997karbakhsh@gmail.com>
2025-07-05 04:08:23 +0330

    Implemented ErrorDisplay Components

--- src/app/reports/create-solo/[id]/page.tsx
+++ src/app/reports/create-solo/[id]/page.tsx
@@ -1,54 +1,26 @@
 "use client";
 
-import Link from "next/link";
 import dynamic from "next/dynamic";
-import { IoIosArrowForward } from "react-icons/io";
-// mui
-import { IconButton } from "@mui/material";
 // _hooks
 import { useGetList } from "./_hooks/useGetList";
 // templates
+import Header from "@/templates/reports/HeaderReports";
+import ErrorDisplay from "@/templates/condition/ErrorDisplay";
 import ConditionSkeleton from "@/templates/reports/condition/ConditionSkeleton";
 const ConditionList = dynamic(() => import("@/templates/reports/condition/ConditionList"));
 
-export default function Calculator() {
-  const { data, isPending, error } = useGetList()
+export default function Reports() {
+  const { data, isPending, error } = useGetList();
 
   return (
     <div className="w-full min-h-screen px-4 py-4 bg-[#f7f7f7]">
       <div className="container mx-auto flex p-3 flex-col justify-start items-center min-w-screen h-full bg-white rounded-xl w-full">
-        <div className="relative flex w-full justify-center items-center h-[52px] rounded-lg bg-[#F7F7FF]">
-          <Link href={`/reports`} className="absolute right-4">
-            <IconButton
-              sx={{
-                borderRadius: "9999px",
-              }}
-            >
-              <IoIosArrowForward fontSize="1.1rem" color="#000" />
-            </IconButton>
-          </Link>
-          ساخت گزارش
-        </div>
+        <Header />
 
-        
         {!error && isPending && <ConditionSkeleton />}
-        {!error && !isPending &&  <ConditionList conditions={data}/>}
-          {error && (
-          <div className="flex flex-col absolute top-[250px] justify-center items-center">
-            <span className="text-red-500">
-              !!خطا در بارگذاری لیست شرط ها
-            </span>
-            <span>{error?.message}</span>
-          </div>
-        )}
+        {!error && !isPending && <ConditionList conditions={data} />}
+        {error && <ErrorDisplay title="گزارش ها" message={error?.message} />}
       </div>
     </div>
-  );    
+  );
 }
-
-
-
-
-
-
-

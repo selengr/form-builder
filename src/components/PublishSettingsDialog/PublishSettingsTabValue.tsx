8c15ac6deb107da6965daa046e29c2f28d977641
Mohammad Javad Soleymani Fard <mjavadsf72@gmail.com>
2025-07-07 17:12:42 +0330

    `feat(ui/components): Update PublishSettingsTabValue component`
    * Update the import statements to use ES6 syntax for better compatibility
    * Remove unused variables and constants to simplify the codebase
    * Enhance accessibility by adding ARIA attributes to the TabIndicator
    
    ⏱️Time spent: [0h 5m]

--- src/components/PublishSettingsDialog/PublishSettingsTabValue.tsx
+++ src/components/PublishSettingsDialog/PublishSettingsTabValue.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { SyntheticEvent, useState } from "react";
-import { Box, Tab, Tabs } from "@mui/material";
+import {SyntheticEvent, useState} from "react";
+import {Box, Tab, Tabs} from "@mui/material";
 import GeneralSettings from "../GeneralSettings/GeneralSettings";
 import IndividualSettings from "../IndividualSettings/IndividualSettings";
 import MresalatUsersSettings from "../MresalatUsersSettings/MresalatUsersSettings";
@@ -9,10 +9,9 @@ import GroupSettings from "../GroupSettings/GroupSettings";
 export type TabValues = "general" | "individual" | "group" | "mresalat";
 
 function CustomTabPanel(props: any) {
-  const { children, value, index, ...other } = props;
+  const {children, value, index, ...other} = props;
 
-  return (
-    <div
+  return (<div
       role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
@@ -20,29 +19,23 @@ function CustomTabPanel(props: any) {
       {...other}
     >
       {value === index && <div>{children}</div>}
-    </div>
-  );
+    </div>);
 }
 
 export default function PublishSettingsTabValue({
-  handleOpen,
-  formId,
-  formData
-}: {
-  handleOpen: () => void;
-  formId: string | number;
-  formData : any;
+                                                  handleOpen, formId, formData
+                                                }: {
+  handleOpen: () => void; formId: string | number; formData: any;
 }) {
-  console.log("formdata",formData);
-  
+  console.log("formdata", formData);
+
   const [value, setValue] = useState<TabValues>("general");
 
   const handleChange = (_: SyntheticEvent, newValue: TabValues) => {
     setValue(newValue);
   };
 
-  return (
-    <>
+  return (<>
       <Box
         sx={{
           borderBottom: 1,
@@ -54,55 +47,41 @@ export default function PublishSettingsTabValue({
         }}
       >
         <Tabs
-          TabIndicatorProps={{ style: { backgroundColor: "#2CDFC9" } }}
+          TabIndicatorProps={{style: {backgroundColor: "#2CDFC9"}}}
           value={value}
           onChange={handleChange}
           scrollButtons
           variant="scrollable"
           sx={{
             "&.MuiTabs-root": {
-              width: "100%",
-              paddingX: { md: "10px", lg: "40px" },
-            },
-            "& .MuiTabs-indicator": {
-              height: "3px",
-              borderRadius: "3px 3px 0 0",
-            },
-            "& .Mui-selected": {
-              color: "#393939 !important",
-              fontWeight: 700,
-            },
-            "& .MuiTabs-flexContainer": {
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
+              width: "100%", paddingX: {md: "10px", lg: "40px"},
+            }, "& .MuiTabs-indicator": {
+              height: "3px", borderRadius: "3px 3px 0 0",
+            }, "& .Mui-selected": {
+              color: "#393939 !important", fontWeight: 700,
+            }, "& .MuiTabs-flexContainer": {
+              display: "flex", justifyContent: "space-between", width: "100%",
             },
           }}
         >
-          <Tab disableRipple label="عمومی" value="general" />
-          <Tab disableRipple label="گروهی" value="group" />
-          <Tab disableRipple label="انفرادی" value="individual" />
-          <Tab disableRipple label="اعضای ام‌رسالت" value="mresalat" />
+          <Tab disableRipple label="عمومی" value="general"/>
+          <Tab disableRipple label="انفرادی" value="individual"/>
+          <Tab disableRipple label="گروهی" value="group" disabled/>
+          <Tab disableRipple label="اعضای ام‌رسالت" value="mresalat" disabled/>
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index="general">
-        {value === "general" && (
-          <GeneralSettings handleOpen={handleOpen} formData={formData} formId={formId as any} />
-        )}
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index="group">
-        {value === "group" && <GroupSettings handleOpen={handleOpen} />}
+        {value === "general" && (<GeneralSettings handleOpen={handleOpen} formData={formData} formId={formId as any}/>)}
       </CustomTabPanel>
       <CustomTabPanel value={value} index="individual">
-        {value === "individual" && (
-          <IndividualSettings handleOpen={handleOpen} />
-        )}
+        {value === "individual" && (<IndividualSettings handleOpen={handleOpen} formId={formId as any}/>)}
       </CustomTabPanel>
+      <CustomTabPanel value={value} index="group">
+        {value === "group" && <GroupSettings handleOpen={handleOpen}/>}
+      </CustomTabPanel>
+
       <CustomTabPanel value={value} index="mresalat">
-        {value === "mresalat" && (
-          <MresalatUsersSettings handleOpen={handleOpen} />
-        )}
+        {value === "mresalat" && (<MresalatUsersSettings handleOpen={handleOpen}/>)}
       </CustomTabPanel>
-    </>
-  );
+    </>);
 }

769ed7b3cecddbb55a407ccf233864f5efb329e6
reza karbakhsh <ui4@fardup.co>
2025-07-07 20:27:27 +0330

    updated delishion hook

--- src/app/reports/create-solo/[id]/_hooks/useDeleteCondition.ts
+++ src/app/reports/create-solo/[id]/_hooks/useDeleteCondition.ts
@@ -1,10 +1,6 @@
 import { toast } from 'sonner';
-import { useRouter } from "next/navigation";
-import { useParams } from 'next/navigation';
 import {AxiosApi} from '@/services/axios/AxiosApi';
-import { useMutation } from '@tanstack/react-query';
-import { queryClient } from '@/lib/react-query.config';
-
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 
 const deleteCalculation = async (id : number) => {
     const url = `/report/solo/${id}`;
@@ -12,23 +8,16 @@ const deleteCalculation = async (id : number) => {
     return response.data;
   };
 
-
 export const useDeleteCondition = () => {
-  const { id } = useParams();
-  const {refresh} = useRouter()
+  const queryClient = useQueryClient(); 
 
   const mutation = useMutation({
     mutationKey: ['delete-condition'],
     mutationFn: (id:number) => deleteCalculation(id),
 
     onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: [`/reports/create-solo/${id}`],
-      });
-      toast.success(`خرده‌گزارش با موفقیت حذف شد`);
-      setTimeout(() => {
-         refresh()
-      }, 500);
+       queryClient.invalidateQueries(['Condition_List'] as any);
+       toast.success(`خرده‌گزارش با موفقیت حذف شد`);
     },
     onError: () => {
       toast.error("عملیات ناموفق بود مجددا تلاش کنید");

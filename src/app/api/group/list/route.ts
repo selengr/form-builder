72df6550f8ee473f94054ae91a1e8dea32bbcc54
Mohammad Javad Soleymani Fard <mjavadsf72@gmail.com>
2025-07-19 16:45:06 +0330

    add env local

--- src/app/api/group/list/route.ts
+++ src/app/api/group/list/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server';
-import { AxiosApi } from '@/services/axios/AxiosApi';
-import { AxiosError } from "axios";
+import {NextResponse} from 'next/server';
+import {AxiosApi} from '@/services/axios/AxiosApi';
+import {AxiosError} from "axios";
 
 interface GroupItem {
   groupName: string;
@@ -43,30 +43,27 @@ export async function GET(request: Request) {
     const token = authorizationHeader?.split(' ')[1];
 
     if (!token) {
-      return NextResponse.json({ error: 'Authorization token is required.' }, { status: 401 });
+      return NextResponse.json({error: 'Authorization token is required.'}, {status: 401});
     }
 
     const url = new URL(request.url);
     const queryString = url.search;
 
-    const { data } = await AxiosApi.get<GroupListgridResponse>(
-      `/user-group/introducer/group-listgrid${queryString}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const {data} = await AxiosApi.get<GroupListgridResponse>(`/user-group/introducer/group-listgrid${queryString}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
     return NextResponse.json(data);
   } catch (error) {
     if (error instanceof AxiosError) {
       const status = error.response?.status || 500;
       const message = error.response?.data?.message || error.message;
-      return NextResponse.json({ error: message }, { status });
+      return NextResponse.json({error: message}, {status});
     } else if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 500 });
+      return NextResponse.json({error: error.message}, {status: 500});
     }
-    return NextResponse.json({ error: 'An unknown error occurred.' }, { status: 500 });
+    return NextResponse.json({error: 'An unknown error occurred.'}, {status: 500});
   }
 }

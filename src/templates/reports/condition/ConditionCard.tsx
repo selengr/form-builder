68ee61610a643454f1e3292bff0ca5cf6da2b7fa
reza karbakhsh <ui4@fardup.co>
2025-07-19 12:26:00 +0330

    remove inline logic

--- src/templates/reports/condition/ConditionCard.tsx
+++ src/templates/reports/condition/ConditionCard.tsx
@@ -2,7 +2,7 @@
 // dnd
 import { CSS } from "@dnd-kit/utilities";
 import { useSortable } from "@dnd-kit/sortable";
-import { useQueryClient} from '@tanstack/react-query';
+import { useQueryClient } from '@tanstack/react-query';
 //services
 import { AxiosApi } from "@/services/axios/AxiosApi";
 // React & Libs
@@ -12,11 +12,11 @@ import { Button, Menu, Typography, CircularProgress } from "@mui/material";
 // types
 import { IConditionCardProps } from "@/types/conditionReportSolo";
 // components
-import ConfirmDialog from "@/components/confirm-dialog";  
+import ConfirmDialog from "@/components/confirm-dialog";
 import { EditConditionDialog } from "./EditConditionDialog";
 import { ConditionCardOperator } from "./ConditionCardOperator";
 // icons
-import {IonCopyOutline} from "@/../public/images/icons/CopyIcon";
+import { IonCopyOutline } from "@/../public/images/icons/CopyIcon";
 import { WeuiDeleteOutlined } from "../../../../public/images/icons/DeleteIcon";
 import { useDeleteReport } from "@/app/reports/create-solo/[id]/_hooks/useDeleteReport";
 import { PhDotsThreeVerticalBold } from "../../../../public/images/icons/PhDotsThreeVerticalBold";
@@ -47,12 +47,13 @@ export function ConditionCard({
   index,
 }: IConditionCardProps) {
   const [open, setOpen] = useState<boolean>(false);
-    const [openEditDialog, setOpenEditDialog] = useState<boolean>(false);
+  const [openEditDialog, setOpenEditDialog] = useState<boolean>(false);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-      const [loadingDuplicateData, setLoadingDuplicateData] = useState(false);
-      // const { removeElement, addElement } = useActionDesigner();
-    const queryClient = useQueryClient();
+  const [loadingDuplicateData, setLoadingDuplicateData] = useState(false);
+
+
+  const queryClient = useQueryClient();
 
   const menuOpen = Boolean(anchorEl);
   const { id } = condition;
@@ -74,7 +75,7 @@ export function ConditionCard({
   };
 
   const handleClick = useCallback((event: any) => {
-        event.stopPropagation();
+    event.stopPropagation();
     setAnchorEl(event.currentTarget);
   }, []);
 
@@ -82,7 +83,7 @@ export function ConditionCard({
     setAnchorEl(null);
   }, []);
 
-    const handleDelete = () => {
+  const handleDelete = () => {
     deleteCondition(id, {
       onSuccess: () => {
         setOpen(false);
@@ -91,17 +92,32 @@ export function ConditionCard({
     });
   };
 
-    const toggleConfirm = () => {
+  const toggleConfirm = () => {
     setOpen((prev) => !prev);
   };
+
+  const handleDuplicate = async (e: any) => {
+    e.stopPropagation();
+    try {
+      setLoadingDuplicateData(true);
+      await AxiosApi.post(`/report/solo/main-list/${id}/duplicate`);
+      queryClient.invalidateQueries(['Report_List'] as any);
+      queryClient.refetchQueries(['Report_List'] as any);
+    } catch (error) {
+      toast.error("خطایی رخ داده است");
+    } finally {
+      setAnchorEl(null);
+      setLoadingDuplicateData(false);
+    }
+  };
+
 
   return (
     <div
       ref={setNodeRef}
       style={style}
-      className={`rounded-lg flex transition-all ${
-        isDragging ? "opacity-50 scale-105" : ""
-      }`}
+      className={`rounded-lg flex transition-all ${isDragging ? "opacity-50 scale-105" : ""
+        }`}
     >
       <div className="flex flex-col justify-start items-center gap-[10px] pl-[10px]">
         <div className="bg-white h-8 w-8 rounded-[10px] flex justify-center items-center">
@@ -134,35 +150,19 @@ export function ConditionCard({
               }}
             >
               <Button
-                                        sx={{
-                                          display: "flex",
-                                          justifyContent: "space-between",
-                                          color: "#222",
-                                          paddingX: "10px",
-                                        }}
-                                        onClick={async (e) => {
-                                          e.stopPropagation();
-                                          try {
-                                            setLoadingDuplicateData(true);
-                                            const res = await AxiosApi.post(
-                                              `/report/solo/main-list/${id}/duplicate`
-                                            );
-                              queryClient.invalidateQueries(['Report_List'] as any);
-                          queryClient.refetchQueries(['Report_List']as any);
-                                          } catch (error) {
-                                            toast.error("خطایی رخ داده است");
-                                          } finally {
-                                            setAnchorEl(null);
-                                            setLoadingDuplicateData(false);
-                                          }
-                                        }}
-                                        fullWidth
-                                        // disabled={loadingDeleteData}
-                                        loading={loadingDuplicateData}
-                                      >
-                                        <Typography>تکثیر</Typography>
-                                        <IonCopyOutline width={18} height={18} />
-                                      </Button>
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  color: "#222",
+                  paddingX: "10px",
+                }}
+                fullWidth
+                onClick={handleDuplicate}
+                loading={loadingDuplicateData}
+              >
+                <Typography>تکثیر</Typography>
+                <IonCopyOutline width={18} height={18} />
+              </Button>
               <Button
                 sx={{
                   paddingX: "10px",
@@ -193,11 +193,11 @@ export function ConditionCard({
                   color: "#FA4D56",
                 }}
                 loading={isPending}
-                         onClick={toggleConfirm}
+                onClick={toggleConfirm}
                 fullWidth
                 disabled={isPending}
               >
-                          <Typography sx={{ fontSize: "12px", color: "black" }}>
+                <Typography sx={{ fontSize: "12px", color: "black" }}>
                   حذف
                 </Typography>
                 <WeuiDeleteOutlined fontSize="1.2rem" />
@@ -210,9 +210,8 @@ export function ConditionCard({
       <div
         {...attributes}
         {...listeners}
-        className={`rounded-lg p-[10px] flex justify-between w-full border-[1px] border-[#1758BA] bg-[#fff] cursor-grab transition-colors active:cursor-grabbing touch-none ${
-          isDragging ? "border-[#CCC]" : "border-[#1758BA]"
-        }`}
+        className={`rounded-lg p-[10px] flex justify-between w-full border-[1px] border-[#1758BA] bg-[#fff] cursor-grab transition-colors active:cursor-grabbing touch-none ${isDragging ? "border-[#CCC]" : "border-[#1758BA]"
+          }`}
       >
         <div className="flex justify-center items-center gap-[10px]">
           <ConditionCardOperator condition={condition} />
@@ -227,39 +226,39 @@ export function ConditionCard({
         />
       )}
 
-        <ConfirmDialog
-              content="آیا از عملیات حذف اطمینان دارید؟"
-              open={open}
-              title="حذف"
-              loading={isPending}
-              onClose={toggleConfirm}
-              cancelText="انصراف"
-              action={
-                <Button
-                  type="submit"
-                  fullWidth
-                  disableRipple
-                  variant="contained"
-                  disabled={isPending}
-                  sx={{ ...buttonStyles, ...buttonStylesError }}
-                  onClick={handleDelete}
-                >
-                  {isPending ? (
-                    <>
-                      <CircularProgress
-                        size={20}
-                        color="inherit"
-                        thickness={5}
-                        style={{ marginLeft: 10 }}
-                      />
-                      در حال حذف…
-                    </>
-                  ) : (
-                    "حذف"
-                  )}
-                </Button>
-              }
-            />
+      <ConfirmDialog
+        content="آیا از عملیات حذف اطمینان دارید؟"
+        open={open}
+        title="حذف"
+        loading={isPending}
+        onClose={toggleConfirm}
+        cancelText="انصراف"
+        action={
+          <Button
+            type="submit"
+            fullWidth
+            disableRipple
+            variant="contained"
+            disabled={isPending}
+            sx={{ ...buttonStyles, ...buttonStylesError }}
+            onClick={handleDelete}
+          >
+            {isPending ? (
+              <>
+                <CircularProgress
+                  size={20}
+                  color="inherit"
+                  thickness={5}
+                  style={{ marginLeft: 10 }}
+                />
+                در حال حذف…
+              </>
+            ) : (
+              "حذف"
+            )}
+          </Button>
+        }
+      />
     </div>
   );
 }

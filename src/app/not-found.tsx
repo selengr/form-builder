72b6a614d0299712e305bde509095ee71fc8a1dc
mjsoleimani <mjavadsf72@gmail.com>
2025-07-23 20:50:20 +0330

    style(ui/not-found): enhance 404 page design
    This commit improves the visual appeal and user experience of the 404 page.
    
    •  Adjusted background and shadow effects for better aesthetics.
    •  Increased border radius of the container.
    •  Enhanced button style for better visibility.
    
    ⏱️Time spent: 0h 15m

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,8 +10,8 @@ export default function NotFound({error}: { error: Error }) {
   }, [error]);
 
   return (
-    <div className="grow flex items-center justify-center bg-[#f9fafb]">
-      <div className="flex flex-col items-center gap-6 p-10 rounded-2xl border border-gray-200 bg-white shadow-xl max-w-sm w-full">
+    <div className="grow flex items-center justify-center bg-[#f9fafbaa]">
+      <div className="flex flex-col items-center gap-6 p-10 rounded-3xl bg-white shadow-2xl shadow-gray-300 max-w-md w-full">
 
         <h2 className="text-gray-800 text-3xl font-semibold text-center font-iran-sans font-d6">مسیر پیدا نشد</h2>
         <p className="text-gray-600 text-center text-base leading-relaxed font-iran-sans font-d6">
@@ -21,8 +21,8 @@ export default function NotFound({error}: { error: Error }) {
         <Button
           variant="contained"
           sx={{
-            borderRadius: "10px",
-            height: "48px",
+            borderRadius: "14px",
+            height: "56px",
             px: "24px",
             fontWeight: "bold",
             backgroundColor: "#2563eb",

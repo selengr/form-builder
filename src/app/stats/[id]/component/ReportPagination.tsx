6ce8c2e9494c23ec515b004994d4eedb3f0b86ae
Mohammad Javad Soleymani Fard <mjavadsf72@gmail.com>
2025-07-23 12:20:28 +0330

    fix file manager route

--- src/app/stats/[id]/component/ReportPagination.tsx
+++ src/app/stats/[id]/component/ReportPagination.tsx
@@ -100,7 +100,7 @@ export function ReportPagination({
                 const fullPath = checkData.filePath.startsWith("/")
                     ? checkData.filePath
                     : `/${checkData.filePath}`;
-                const downloadUrl = `${window.location.origin}${fullPath}`;
+                const downloadUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/filemanager${fullPath}`;
                 window.open(downloadUrl, "_blank");
                 toast.success("فایل آماده بود و دانلود شد.");
             } else {

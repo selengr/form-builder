3ea04d8ae1ea4ff182c0c85e77c56816c104946b
reza karbakhsh <ui4@fardup.co>
2025-07-26 21:54:37 +0330

    conditional displayng report list for super admin fix2

--- src/templates/reports/HeaderReports.tsx
+++ src/templates/reports/HeaderReports.tsx
@@ -1,16 +1,16 @@
-import {IconButton} from "@mui/material";
-import {IoIosArrowForward} from "react-icons/io";
+import { IconButton } from "@mui/material";
+import { IoIosArrowForward } from "react-icons/io";
 import { useRouter, useSearchParams } from "next/navigation";
 
 const Header = () => {
   const { back } = useRouter()
-    const searchParams = useSearchParams()
+  const searchParams = useSearchParams()
   const search = searchParams.get('rep')
   const admin = search === "list"
   const name = searchParams.get('name')
-  
+
   return (
-    <div className="relative flex w-full justify-center items-center h-[52px] rounded-lg bg-[#F7F7FF]">
+    <div className={`relative flex w-full justify-center items-center h-[52px] rounded-lg bg-[#F7F7FF] ${admin ? "mb-8" : ""}`}>
       <div onClick={back} className="absolute right-4">
         <IconButton
           sx={{
@@ -20,7 +20,7 @@ const Header = () => {
           <IoIosArrowForward fontSize="1.1rem" color="#000" />
         </IconButton>
       </div>
-        {admin ? `گزارش فرم ${name}` : " ساخت گزارش"}
+      {admin ? `گزارش فرم ${name}` : " ساخت گزارش"}
     </div>
   );
 };

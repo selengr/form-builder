b76c49fb908e688272f3827b8ddefb98bb5d7ef8
Mohammad Javad Soleymani Fard <mjavadsf72@gmail.com>
2025-07-27 14:05:01 +0330

    feat(conditional): enhance condition formula generation
    This commit improves the condition formula generation to handle different operator types and ensure correct formatting of values.
    
    - Update condition formula generation to handle numeric elseQuestionId and returnQuestionId.
    - Ensure correct formatting for option, value, and text operator types.
    - Improve handling of logical operators in sub-conditions.
    
    ⏱️Time spent: [1h 30m]

--- src/templates/condition/ConditionalSystem.tsx
+++ src/templates/condition/ConditionalSystem.tsx
@@ -20,210 +20,181 @@ import {useGetOnlyAllQuestions} from "@/app/(builder)/builder/[id]/condition/_ho
 import {useGetOnlyAllCalculation} from "@/app/(builder)/builder/[id]/condition/_hooks/useGetOnlyAllCalculation"
 
 export const ConditionalSystem: React.FC<IConditionalSystemProps> = ({
-  handleClose,
-  condition,
-  isEdit = false
-}) => {
-  const { id } = useParams();
-  const {refresh} = useRouter()
-  const {
-    methods,
-    conditions,
-    handleAddCondition,
-    handleRemoveCondition,
-    handleAddSubCondition,
-    handleRemoveSubCondition,
-  } = useConditionalForm(condition)
+                                                                         handleClose, condition, isEdit = false
+                                                                     }) => {
+    const {id} = useParams();
+    const {refresh} = useRouter()
+    const {
+        methods, conditions, handleAddCondition, handleRemoveCondition, handleAddSubCondition, handleRemoveSubCondition,
+    } = useConditionalForm(condition)
 
-  const { qacWithOutFilterOptions, isFetchingQacWithOutFilter } = useGetQacWithOutFilter()
-  const {
-     onlyAllQuestions,
-     onlyAllDateOptions,
-     onlyAllQuestionsOptions,
-      onlySomeQuestionsOptions,
-       isFetchingOnlyAllQuestions
-       } = useGetOnlyAllQuestions()
-  const { onlyAllCalculationOptions, isFetchingOnlyAllCalculation } = useGetOnlyAllCalculation()
+    const {qacWithOutFilterOptions, isFetchingQacWithOutFilter} = useGetQacWithOutFilter()
+    const {
+        onlyAllQuestions,
+        onlyAllDateOptions,
+        onlyAllQuestionsOptions,
+        onlySomeQuestionsOptions,
+        isFetchingOnlyAllQuestions
+    } = useGetOnlyAllQuestions()
+    const {onlyAllCalculationOptions, isFetchingOnlyAllCalculation} = useGetOnlyAllCalculation()
 
-  const postCondition = usePostCondition(isEdit);
+    const postCondition = usePostCondition(isEdit);
 
-  const onSubmit = (input: TConditionFormData) => {
+    const onSubmit = (input: TConditionFormData) => {
 
-    const transformInputToOutput = (input : TConditionFormData) : any => {
-      return input.conditions.map((condition : TConditionData,index) => {
-        const { subConditions, returnQuestionId, elseQuestionId } = condition;
+        const transformInputToOutput = (input: TConditionFormData): any => {
+            return input.conditions.map((condition: TConditionData, index) => {
+                const {subConditions, returnQuestionId, elseQuestionId} = condition;
 
-        const conditionFormula = subConditions
-          .map((subCondition : TSubConditionData) => {
-            const conditionType = subCondition.conditionType?.split("@")[0];
-            const questionType = subCondition.questionType?.split("@")[0];
-            const operatorType = subCondition.operatorType?.split("@")[0];
-            const value = typeof subCondition.value !== 'object' ? subCondition.value?.split("@")[0] : subCondition.value;
-            const logicalOperator = subCondition.logicalOperator?.split("@")[0];
+                const conditionFormula = subConditions
+                    .map((subCondition: TSubConditionData) => {
+                        const conditionType = subCondition.conditionType?.split("@")[0];
+                        const questionType = subCondition.questionType?.split("@")[0];
+                        const operatorType = subCondition.operatorType?.split("@")[0];
+                        const value = typeof subCondition.value !== 'object' ? subCondition.value?.split("@")[0] : subCondition.value;
+                        const logicalOperator = subCondition.logicalOperator?.split("@")[0];
 
-            let formattedValue: string;
+                        let formattedValue: string;
 
-            if (operatorType === "OPTION") {
-              if(typeof subCondition.value === 'object'){
-                formattedValue = `{${Array.isArray(value) && value?.map((item:string)=>item?.split("@")[0])}}`
-              } else formattedValue = `{${value}}`;
-            } else if (operatorType === "VALUE") {
-              formattedValue = `{#v_${value}}`;
-            } else if (operatorType === "TEXT") {
-              if (
-                conditionType === "#startWithText" ||
-                conditionType === "#endWithText"
-              ) {
-                formattedValue = `{"${value}"}`;
-              } else if (
-                conditionType === "!#containAnyText" ||
-                conditionType === "#containAnyText"
-              ) {
-                formattedValue = `{${formatContainText(value as string)}}`;
-              } else if (
-                conditionType === "#lenEqualText" ||
-                conditionType === "#lenGraterThanText" ||
-                conditionType === "!#lenGraterThanText"
-              ) {
-                formattedValue = `{#v_${value}}`;
-              } else {
-                formattedValue = value as string;
-              }
-            } else if (operatorType === "DATE") {
-              formattedValue = `{#v_"${value}"}`;
-            } else {
-              formattedValue = value as string;
-            }
+                        if (operatorType === "OPTION") {
+                            if (typeof subCondition.value === 'object') {
+                                formattedValue = `{${Array.isArray(value) && value?.map((item: string) => item?.split("@")[0])}}`
+                            } else formattedValue = `{${value}}`;
+                        } else if (operatorType === "VALUE") {
+                            formattedValue = `{#v_${value}}`;
+                        } else if (operatorType === "TEXT") {
+                            if (conditionType === "#startWithText" || conditionType === "#endWithText") {
+                                formattedValue = `{"${value}"}`;
+                            } else if (conditionType === "!#containAnyText" || conditionType === "#containAnyText") {
+                                formattedValue = `{${formatContainText(value as string)}}`;
+                            } else if (conditionType === "#lenEqualText" || conditionType === "#lenGraterThanText" || conditionType === "!#lenGraterThanText") {
+                                formattedValue = `{#v_${value}}`;
+                            } else {
+                                formattedValue = value as string;
+                            }
+                        } else if (operatorType === "DATE") {
+                            formattedValue = `{#v_"${value}"}`;
+                        } else {
+                            formattedValue = value as string;
+                        }
 
-            const baseCondition = `${conditionType}(${
-              questionType.split("*")[1]
-            },${formattedValue})`;
+                        const baseCondition = `${conditionType}(${questionType.split("*")[1]},${formattedValue})`;
 
-            return logicalOperator
-              ? ` ${logicalOperator} ${baseCondition}`
-              : baseCondition;
-          })
-          .join("");
+                        return logicalOperator ? ` ${logicalOperator} ${baseCondition}` : baseCondition;
+                    })
+                    .join("");
 
-        return {
-          formBuilderId: Number(id),
-          conditionFormula: conditionFormula,
-          elseQuestionId: Number(elseQuestionId.replace(/\D/g, '')) !== 0 ? Number(elseQuestionId.replace(/\D/g, '')) : null,
-          returnQuestionId: Number(returnQuestionId.replace(/\D/g, '')),
-          frontConditionData: JSON.stringify(input.conditions[index]),
-          ...(isEdit && { id: Number(condition.id) })
+                return {
+                    formBuilderId: Number(id),
+                    conditionFormula: conditionFormula,
+                    elseQuestionId: (() => {
+                        const match = elseQuestionId.match(/\d+/);
+                        return match ? Number(match[0]) : null;
+                    })(),
+                    returnQuestionId: (() => {
+                        const match = returnQuestionId.match(/\d+/);
+                        return match ? Number(match[0]) : null;
+                    })(),
+                    frontConditionData: JSON.stringify(input.conditions[index]), ...(isEdit && {id: Number(condition.id)}),
+                };
+            });
         };
-      });
+
+        const output: IPostCondition[] = transformInputToOutput(input);
+        postCondition.mutate({data: output}, {
+            onSuccess: async () => {
+                handleClose()
+            }, onError: (error: any) => {
+                // ...
+            },
+        });
     };
 
-    const output : IPostCondition[] = transformInputToOutput(input);
-    postCondition.mutate(
-      { data: output },
-      {
-        onSuccess: async () => {
-          handleClose()
-        },
-        onError: (error: any) => {
-          // ...
-        },
-      }
-    );
-  };
-
-  return (
-    <Box
-      sx={{ width: "100%",  display: "flex", flexDirection: "column", justifyContent: "center", direction: "ltr" }}
-    >
-      <Typography
-        variant="subtitle1"
-        sx={{ display: "flex", justifyContent: "center", color: "#404040", fontWeight: 700, mb: 1 }}
-      >
-        شرط
-      </Typography>
-      <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          {conditions.map((condition, index) => (
-            <Box key={condition.id} sx={{ width: "100%" }}>
-              {condition.subConditions.map((subCondition, subIndex) => (
-                <SubCondition
-                  key={subCondition.id}
-                  index={index}
-                  subIndex={subIndex}
-                  onAddSubCondition={() => handleAddSubCondition(index, subIndex)}
-                  onRemoveSubCondition={() => handleRemoveSubCondition(index, subIndex)}
-                  qacWithOutFilterOptions={qacWithOutFilterOptions}
-                  isFetchingQacWithOutFilter={isFetchingQacWithOutFilter}
-                  onlySomeQuestionsOptions={onlySomeQuestionsOptions}
-                  isFetchingOnlyAllQuestions={isFetchingOnlyAllQuestions}
-                  onlyAllCalculationOptions={onlyAllCalculationOptions}
-                  isFetchingOnlyAllCalculation={isFetchingOnlyAllCalculation}
-                  onlyAllQuestions={onlyAllQuestions}
-                  onlyAllDateOptions={onlyAllDateOptions}
-                />
-              ))}
-              <Box
-                sx={{
-                  ml: { xs: 0, md: 2 },
-                  display: "flex",
-                  alignItems: "center",
-                  gap: 1,
-                  flexWrap: "wrap",
-                  flexDirection: { xs: "column", md: "row" },
-                }}
-              >
-                <Typography sx={{ color: "#393939", fontSize: "14px" }}>برو به:</Typography>
-                <SelectController
-                  name={`conditions.${index}.returnQuestionId`}
-                  options={onlyAllQuestionsOptions}
-                  isLoading={isFetchingOnlyAllQuestions}
-                  sx={{ minWidth: 240, ml: 2.5 }}
-                />
-                <Typography sx={{ color: "#393939", fontSize: "14px", mr: 3.5 }}>در غیر اینصورت برو به:</Typography>
-                <SelectController
-                  name={`conditions.${index}.elseQuestionId`}
-                  options={onlyAllQuestionsOptions}
-                  isLoading={isFetchingOnlyAllQuestions}
-                  sx={{ minWidth: 300, width: 380 }}
-                />
-                {index !== 0 && (
-                <Button
-                  onClick={() => handleRemoveCondition(index)}
-                  sx={{
-                    width: 113,
-                    height: "50px",
-                    bgcolor: "#FA4D560D",
-                    borderRadius: "8px",
-                    border: "1px solid #FA4D56",
-                    "&:hover": { bgcolor: "#FA4D560D" },
-                  }}
-                >
-                  <Typography sx={{ color: "#FA4D56", fontSize: "14px" }}>حذف این شرط</Typography>
-                </Button>
-            )}
-              </Box>
-              <CircleDivider />
-            </Box>
-          ))}
-          {!isEdit && (
-            <Button
-            variant="outlined"
-            onClick={handleAddCondition}
-            sx={{
-              ml: 2,
-              height: 50,
-              maxWidth: 155,
-              color: "white",
-              bgcolor: "#1758BA",
-              borderRadius: "8px",
-            }}
-          >
-            افزودن شرط جدید
-          </Button>
-          )}
-          <SubmitButtons isLoading={postCondition.isPending} handleClose={handleClose}/>
-        </form>
-      </FormProvider>
-    </Box>
-  )
+    return (<Box
+            sx={{width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", direction: "ltr"}}
+        >
+            <Typography
+                variant="subtitle1"
+                sx={{display: "flex", justifyContent: "center", color: "#404040", fontWeight: 700, mb: 1}}
+            >
+                شرط
+            </Typography>
+            <FormProvider {...methods}>
+                <form onSubmit={methods.handleSubmit(onSubmit)}>
+                    {conditions.map((condition, index) => (<Box key={condition.id} sx={{width: "100%"}}>
+                            {condition.subConditions.map((subCondition, subIndex) => (<SubCondition
+                                    key={subCondition.id}
+                                    index={index}
+                                    subIndex={subIndex}
+                                    onAddSubCondition={() => handleAddSubCondition(index, subIndex)}
+                                    onRemoveSubCondition={() => handleRemoveSubCondition(index, subIndex)}
+                                    qacWithOutFilterOptions={qacWithOutFilterOptions}
+                                    isFetchingQacWithOutFilter={isFetchingQacWithOutFilter}
+                                    onlySomeQuestionsOptions={onlySomeQuestionsOptions}
+                                    isFetchingOnlyAllQuestions={isFetchingOnlyAllQuestions}
+                                    onlyAllCalculationOptions={onlyAllCalculationOptions}
+                                    isFetchingOnlyAllCalculation={isFetchingOnlyAllCalculation}
+                                    onlyAllQuestions={onlyAllQuestions}
+                                    onlyAllDateOptions={onlyAllDateOptions}
+                                />))}
+                            <Box
+                                sx={{
+                                    ml: {xs: 0, md: 2},
+                                    display: "flex",
+                                    alignItems: "center",
+                                    gap: 1,
+                                    flexWrap: "wrap",
+                                    flexDirection: {xs: "column", md: "row"},
+                                }}
+                            >
+                                <Typography sx={{color: "#393939", fontSize: "14px"}}>برو به:</Typography>
+                                <SelectController
+                                    name={`conditions.${index}.returnQuestionId`}
+                                    options={onlyAllQuestionsOptions}
+                                    isLoading={isFetchingOnlyAllQuestions}
+                                    sx={{minWidth: 240, ml: 2.5}}
+                                />
+                                <Typography sx={{color: "#393939", fontSize: "14px", mr: 3.5}}>در غیر این صورت برو
+                                    به:</Typography>
+                                <SelectController
+                                    name={`conditions.${index}.elseQuestionId`}
+                                    options={onlyAllQuestionsOptions}
+                                    isLoading={isFetchingOnlyAllQuestions}
+                                    sx={{minWidth: 300, width: 380}}
+                                />
+                                {index !== 0 && (<Button
+                                        onClick={() => handleRemoveCondition(index)}
+                                        sx={{
+                                            width: 113,
+                                            height: "50px",
+                                            bgcolor: "#FA4D560D",
+                                            borderRadius: "8px",
+                                            border: "1px solid #FA4D56",
+                                            "&:hover": {bgcolor: "#FA4D560D"},
+                                        }}
+                                    >
+                                        <Typography sx={{color: "#FA4D56", fontSize: "14px"}}>حذف این شرط</Typography>
+                                    </Button>)}
+                            </Box>
+                            <CircleDivider/>
+                        </Box>))}
+                    {!isEdit && (<Button
+                            variant="outlined"
+                            onClick={handleAddCondition}
+                            sx={{
+                                ml: 2,
+                                height: 50,
+                                maxWidth: 155,
+                                color: "white",
+                                bgcolor: "#1758BA",
+                                borderRadius: "8px",
+                            }}
+                        >
+                            افزودن شرط جدید
+                        </Button>)}
+                    <SubmitButtons isLoading={postCondition.isPending} handleClose={handleClose}/>
+                </form>
+            </FormProvider>
+        </Box>)
 }
 

9f1dee48cf326c1073cecde0b7c519bcdbb575f3
Mohammad Javad Soleymani Fard <mjavadsf72@gmail.com>
2025-07-29 12:48:41 +0330

    implement new landing

---
+++ src/app/L/page.tsx
@@ -0,0 +1,8 @@
+import HomePage from "@/templates/homePage/home";
+import HomePagex from "@/templates/homePage/home2";
+
+export const dynamic = 'force-dynamic';
+
+export default async function Home() {
+    return <HomePagex/>;
+}

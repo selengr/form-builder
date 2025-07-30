3d0972cce1f699b44b330e31c1e404ea6444d45c
Mohammad Javad Soleymani Fard <mjavadsf72@gmail.com>
2025-07-30 08:13:06 +0330

    refactor(middle-sidebar): improve middle sidebar layout
    This commit refactors the middle sidebar to improve its layout and responsiveness.
    
    - Make logo always visible
    - Use flex-grow to allow content to fill the available space
    - Improve visual appearance
    
    ⏱️Time spent: [1h 0m]

--- src/components/MiddleSidebar/MiddleSidebar.tsx
+++ src/components/MiddleSidebar/MiddleSidebar.tsx
@@ -14,24 +14,25 @@ export default function MiddleSidebar() {
   }, [menu?.aclList]);
 
   return (
-    <div
-    className="min-w-[400px] w-[400px] min-h-screen bg-white px-5 py-5 flex flex-col gap-8 overflow-y-auto"
-    style={{scrollbarWidth: "thin"}}
-  >
-    <div className="w-full flex flex-col gap-5 items-center">
-      <Image
-        src={`/api/images?folder=logo&file=LOGO.svg`}
-        width={111} height={38} alt="Psya-Logo" priority draggable={false}
-      />
-      <div className="flex flex-col items-center w-full gap-5">
-        {/*<SidebarRoleSelection />*/}
-        <div className="mt-7"/>
-        <div className="w-full pr-3 flex flex-col gap-4">
-          {!!userInfo && loading && <MenuItemSkeleton/>}
-          <MenuList menuLinks={menuLinks}/>
+      <div className="min-w-[400px] w-[400px] min-h-screen bg-white px-5 py-5 flex flex-col gap-8">
+        <div className="flex flex-col gap-5 items-center flex-shrink-0">
+          <Image
+              src={`/api/images?folder=logo&file=LOGO.svg`}
+              width={150}
+              height={38}
+              alt="Psya-Logo"
+              priority
+              draggable={false}
+          />
+        </div>
+
+        <div
+            className="flex-1 overflow-y-auto pr-3 flex flex-col gap-4"
+            style={{ scrollbarWidth: "thin" }}
+        >
+          {!!userInfo && loading && <MenuItemSkeleton />}
+          <MenuList menuLinks={menuLinks} />
         </div>
       </div>
-    </div>
-  </div>
   );
 }

e9d1a9d63398cd8b79a661145d8887b9b80d5580
Mohammad Javad Soleymani Fard <mjavadsf72@gmail.com>
2025-08-03 10:20:13 +0330

    refactor(api): consolidate api request handling
    This commit refactors the API request handling logic to improve code reusability and maintainability.
    
    - Standardize error handling across API endpoints.
    - Simplify request construction and authorization.
    - Improve code readability.
    
    ⏱️Time spent: [1h 30m]

--- src/app/api/report/check/[id]/route.ts
+++ src/app/api/report/check/[id]/route.ts
@@ -1,4 +1,4 @@
-import { handleDynamicGetRequest } from '../../../helper';
+import { handleGetRequest } from '../../../helper';
 
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
@@ -7,5 +7,5 @@ export const revalidate = 0;
 export async function GET(req: Request, context: { params: { id: string } }) {
     const { id } = context.params;
     const endpoint = `/psya/report/solo/main-list/excel-export/check/${id}`;
-    return handleDynamicGetRequest(req, endpoint);
+    return handleGetRequest(req, endpoint);
 }

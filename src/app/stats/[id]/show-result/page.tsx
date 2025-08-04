1b13cb1ef35b1f16b5d3441379d7b54785b1d5c0
reza karbakhsh <ui4@fardup.co>
2025-08-04 13:38:45 +0330

    debug result show page

--- src/app/stats/[id]/show-result/page.tsx
+++ src/app/stats/[id]/show-result/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import Image from "next/image";
-import {useParams} from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 import {IconButton} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import {IoIosArrowForward} from "react-icons/io";
@@ -17,6 +17,9 @@ const ResultsPage = () => {
   const { id } = useParams()
   const [results, setResults] = useState<Result[]>([]);
 
+    const searchParams = useSearchParams()
+    const search = searchParams.get('name')
+
   useEffect(() => {
     const storedResults = localStorage.getItem('testResult');
     if (storedResults) {
@@ -26,9 +29,9 @@ const ResultsPage = () => {
 
 
   return (
-      <div className="w-full min-h-screen px-4 py-4 bg-[#f7f7f7]">
-        <div className="md:container mx-auto flex p-3 flex-col justify-start items-center min-w-screen h-full bg-white rounded-xl w-full">
-          <div className="relative flex w-full justify-center items-center h-[52px] rounded-lg bg-[#F7F7FF]">
+      <div className="w-full min-h-screen h-full  px-4 py-4 bg-[#f7f7f7]">
+        <div className="md:container mx-auto flex p-3 flex-col justify-start items-center min-w-screen h-full bg-white rounded-xl w-full overflow-y-scroll">
+          <div className="relative flex w-full justify-center items-center min-h-[52px] h-[52px] rounded-lg bg-[#F7F7FF]">
             <Link href={`/stats/${id}`} className="absolute right-4">
               <IconButton
                   sx={{
@@ -38,7 +41,7 @@ const ResultsPage = () => {
                 <IoIosArrowForward fontSize="1.1rem" color="#000" />
               </IconButton>
             </Link>
-            <span className="text-[#161616]">گزارش فرم </span>
+            <span className="text-[#161616]">گزارش فرم {search}</span>
           </div>
 
           <Image
@@ -48,10 +51,10 @@ const ResultsPage = () => {
           height={250}
           priority
           draggable={false}
-          className="w-full sm:w-[50%] lg:w-[500px]"
+          className="w-full sm:w-[50%] lg:w-[450px]"
         />
 
-          <div className="p-8 mt-10 max-w-[600px]">
+          <div className="p-8 pt-0 max-w-[600px]">
             {results?.map((result, index) => (
                 <div key={index} className="mb-4 last:mb-0">
                   {/* <h2 className="text-right text-[15px] font-bold text-[#161616] mb-1"></h2> */}

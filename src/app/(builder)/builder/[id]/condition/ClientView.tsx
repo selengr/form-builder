bc40b46696a501ac6985f1594cb7039d464d0eb6
reza karbakhsh <ui4@fardup.co>
2025-08-05 16:16:53 +0330

    updated condition list

--- src/app/(builder)/builder/[id]/condition/ClientView.tsx
+++ src/app/(builder)/builder/[id]/condition/ClientView.tsx
@@ -19,8 +19,8 @@ export default function ClientView<T>({
   error,
 }: IProps<T>) {
   return (
-    <div className="w-full min-h-screen px-4 py-4">
-      <div className=" mx-auto flex flex-col min-h-[calc(100vh-100px)] md:min-h-[calc(100vh-40px)] justify-start items-center bg-white rounded-xl w-full">
+    <div className="w-full h-screen px-4 py-4">
+      <div className="md:container mx-auto flex pb-3 flex-col  min-w-screen h-full justify-start items-center bg-white rounded-xl w-full">
         <DesignerTabs />
 
         {!error && isPending && <ConditionSkeleton />}
--- src/templates/condition/ConditionList.tsx
+++ src/templates/condition/ConditionList.tsx
@@ -1,12 +1,12 @@
 "use client";
 // lib
-import {idGenerator} from "@/lib";
+import { idGenerator } from "@/lib";
 // hooks
-import {useFormData} from "@/hooks";
+import { useFormData } from "@/hooks";
 // types
-import {IGetCondition} from "@/types/condition";
+import { IGetCondition } from "@/types/condition";
 // view
-import {ConditionCard} from "./ConditionCard";
+import { ConditionCard } from "./ConditionCard";
 import CreateCondition from "./CreateCondition";
 
 interface IProps {
@@ -17,28 +17,35 @@ const ConditionList: React.FC<IProps> = ({ conditions }) => {
   const { formData, isLoading } = useFormData();
 
   return (
-    <div className="w-full max-w-[520px] flex flex-col p-[13px] overflow-hidden">
-      {formData?.formSettingModel?.formStatus === "CREATE" && (
-        <CreateCondition />
-      )}
-      {Array.isArray(conditions) && conditions.length > 0 && (
-        <div
-          dir="rtl"
-          className="bg-[#F7F7FF] rounded-lg p-[10px] w-full flex flex-col gap-3  mb-10 overflow-y-auto"
-        >
-          {conditions?.map((condition: IGetCondition, index: number) => (
-            <ConditionCard
-              key={idGenerator()}
-              condition={condition}
-              index={index}
-              disabled={
-                isLoading || formData?.formSettingModel?.formStatus !== "CREATE"
-              }
-            />
-          ))}
+    <>
+      <div className="w-full max-w-[530px] -mr-3">
+        {formData?.formSettingModel?.formStatus === "CREATE" && (
+          <CreateCondition />
+        )}
+      </div>
+      <div dir="rtl" className="w-full overflow-y-auto h-full flex justify-center">
+        <div className="w-full max-w-[520px] flex flex-col p-[13px]">
+
+          {Array.isArray(conditions) && conditions.length > 0 && (
+            <div
+              dir="rtl"
+              className="bg-[#F7F7FF] rounded-lg p-[10px] w-full flex flex-col gap-3  mb-10"
+            >
+              {conditions?.map((condition: IGetCondition, index: number) => (
+                <ConditionCard
+                  key={idGenerator()}
+                  condition={condition}
+                  index={index}
+                  disabled={
+                    isLoading || formData?.formSettingModel?.formStatus !== "CREATE"
+                  }
+                />
+              ))}
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 

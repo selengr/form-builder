feda34d9c2bf379727e5e2894bd3f5e91d9b1c41
Mohammad Javad Soleymani Fard <mjavadsf72@gmail.com>
2025-08-11 08:47:12 +0330

    feat(my-assessment): disable button after submission
    Disable the button to prevent multiple submissions.
    
    - Prevent users from clicking the button multiple times.
    - Improve user experience by indicating that the action is complete.
    
    ⏱️Time spent: [Xh Xm]

--- src/templates/my-assessment/ListCard.tsx
+++ src/templates/my-assessment/ListCard.tsx
@@ -2,5 +2,5 @@
 import FormCardBase from '@/components/common/FormCardBase';
 
 export default function ListCard(props: any) {
-  return <FormCardBase data={props.data} buttonText='مشاهده پیش‌نمایش' buttonLink={`/preview/${props.data.id}`} />;
+  return <FormCardBase data={props.data} buttonText='مشاهده پیش‌نمایش' buttonLink={`/preview/${props.data.id}`} buttonDisabled/>;
 }

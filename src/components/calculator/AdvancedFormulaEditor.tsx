bb31a085ce965e4706aa26e63fe1029264f8b60a
reza karbakhsh <ui4@fardup.co>
2025-08-26 21:39:16 +0330

    debug combo position

--- src/components/calculator/AdvancedFormulaEditor.tsx
+++ src/components/calculator/AdvancedFormulaEditor.tsx
@@ -73,7 +73,7 @@ const AdvancedFormulaEditor: React.FC<IAdvancedFormulaEditorProps> = ({ question
 
     elements.forEach((elem) => {
       if (elem.type === 'NEW_FIELD') {
-        questionList.dataList.forEach((item: any) => {
+        questionList.dataList.forEach((item: any) => {debugger
           const { UNIC_NAME, STICKY_FUNC } = item.extMap;
           if (UNIC_NAME === elem.id || STICKY_FUNC === elem.id) {
             selectFieldRef.current[elem.id as string] = STICKY_FUNC || UNIC_NAME;
@@ -376,11 +376,9 @@ const AdvancedFormulaEditor: React.FC<IAdvancedFormulaEditorProps> = ({ question
     const { UNIC_NAME, STICKY_FUNC } = item.extMap;
     const finalId = STICKY_FUNC ?? UNIC_NAME;
 
-    const elementIndex = elements.findIndex((elem) => elem.id === dropdownId);
-    if (elementIndex === -1) return;
     const newElements = [...elements];
 
-    newElements[elementIndex] = {
+    newElements[element.mainIndex] = {
       type: 'NEW_FIELD',
       content: item.caption,
       id: finalId,
@@ -391,8 +389,8 @@ const AdvancedFormulaEditor: React.FC<IAdvancedFormulaEditorProps> = ({ question
     setElements(newElements);
     selectFieldRef.current[finalId] = finalId;
 
-    setCursorIndex(elementIndex + 1);
-    updateCursorPosition(elementIndex + 1);
+    setCursorIndex(element.mainIndex + 1);
+    updateCursorPosition(element.mainIndex + 1);
     closeDropdown(dropdownId);
   };
 

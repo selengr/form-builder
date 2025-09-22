12c03dfa5a004a82eff6f545c5d228a380e25d3f
reza karbakhsh <ui4@fardup.co>
2025-09-22 16:39:33 +0330

    complition of implementing report in my-asseccments last update

--- src/validators/destroyReportSchema.ts
+++ src/validators/destroyReportSchema.ts
@@ -5,7 +5,7 @@ export const destroyReportSchema = z.object({
   description: z.string().min(1),
   username: z.string().min(5),
   responseForDestroyerReport: z.enum(['PRIVACY_VIOLATION', 'INTELLECTUAL_PROPERTY_INFRINGMENT', 'INAPPROPRIATE_CONTENT', 'OTHER']),
-  typeOfReport: z.enum(['REPORT', 'FORM']),
+  typeOfReport: z.enum(['REPORT', 'FORM', 'RESULT_REPORT']),
   questionId: z.number().optional(),
   resultReportText: z.string().optional(),
 });

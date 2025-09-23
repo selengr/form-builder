bce3dbd7112180723d4d3c6bb3a94be6f8d206fb
reza karbakhsh <ui4@fardup.co>
2025-09-23 15:31:11 +0330

    resolve issue number #30

--- src/app/(builder)/user-reports/_component/ReportTable.tsx
+++ src/app/(builder)/user-reports/_component/ReportTable.tsx
@@ -60,10 +60,21 @@ export function ReportTable({ headData, allData, isLoading }: StatsTableProps) {
               <tr key={rowIndex} className={rowIndex % 2 !== 0 ? 'bg-[#F7F7FF]' : 'bg-white'}>
                 <TableCell content={rowIndex + 1} rowIndex={rowIndex} />
                 <TableCell content={row.formName} rowIndex={rowIndex} />
-                <TableCell content={row.reporterInformation} rowIndex={rowIndex} />
+                <TableCell content={row.userFullName} rowIndex={rowIndex} />
                 <TableCell content={row.numberOfReportingPoints} rowIndex={rowIndex} />
-                <TableCell content={row.publicationApprovalByAdmin} rowIndex={rowIndex} />
-
+                <TableCell
+                  content={
+                    <span
+                      className={`px-3 py-1 rounded-full text-xs font-medium ${row.publicationApprovalByAdmin
+                          ? 'bg-green-100 text-green-700'
+                          : 'bg-red-100 text-red-700'
+                        }`}
+                    >
+                      {row.publicationApprovalByAdmin ? 'فعال' : 'غیرفعال'}
+                    </span>
+                  }
+                  rowIndex={rowIndex}
+                />
                 {/* ستون عملیات */}
                 <td className='px-4 py-2 border-l-0 border-slate-300 align-middle'>
                   <div className='flex items-center justify-center gap-2 align-middle'>

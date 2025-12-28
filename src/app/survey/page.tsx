75cf079fffa918e65a4fb089fed6dd7b8eb963cf
Reza Karbakhsh 1640 <ui4@fardup.co>
2025-12-28 23:22:47 +0330

    Improve survey load page time => LCP & INP & CLS

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,45 +1,44 @@
-// import { Suspense } from 'react';
-// import ListGridWrapper from '@/templates/survey/ListGridWrapper';
-
-// export default function MyAssessmentPage() {
-//   return (
-//     <Suspense>
-//       <ListGridWrapper />
-//     </Suspense>
-//   );
-// }
-
-
+import { Suspense } from 'react';
 import ListGridWrapper from '@/templates/survey/ListGridWrapper';
 
-import {
-  HydrationBoundary,
-  QueryClient,
-  dehydrate,
-} from '@tanstack/react-query';
-
-import { fetchSurveyData, SURVEY_PURPOSE_QUERY_KEY } from '@/templates/survey/hooks/useGetSurveyPurpose';
-import { fetchTargetPlatformData, TARGET_PLATFORM_QUERY_KEY } from '@/templates/survey/hooks/useGetTargetPlatform';
-
-export const dynamic = 'force-dynamic';
-
-export default async function SurveyPage() {
-  const queryClient = new QueryClient();
-
-  await Promise.all([
-    queryClient.prefetchQuery({
-      queryKey: SURVEY_PURPOSE_QUERY_KEY,
-      queryFn: fetchSurveyData,
-    }),
-    queryClient.prefetchQuery({
-      queryKey: TARGET_PLATFORM_QUERY_KEY,
-      queryFn: fetchTargetPlatformData,
-    }),
-  ]);
-
+export default function SurveyPage() {
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
+    <Suspense>
       <ListGridWrapper />
-    </HydrationBoundary>
+    </Suspense>
   );
 }
+
+
+
+// import {
+//   HydrationBoundary,
+//   QueryClient,
+//   dehydrate,
+// } from '@tanstack/react-query';
+
+// import { fetchSurveyData, SURVEY_PURPOSE_QUERY_KEY } from '@/templates/survey/hooks/useGetSurveyPurpose';
+// import { fetchTargetPlatformData, TARGET_PLATFORM_QUERY_KEY } from '@/templates/survey/hooks/useGetTargetPlatform';
+
+// export const dynamic = 'force-dynamic';
+
+// export default async function SurveyPage() {
+//   const queryClient = new QueryClient();
+
+//   await Promise.all([
+//     queryClient.prefetchQuery({
+//       queryKey: SURVEY_PURPOSE_QUERY_KEY,
+//       queryFn: fetchSurveyData,
+//     }),
+//     queryClient.prefetchQuery({
+//       queryKey: TARGET_PLATFORM_QUERY_KEY,
+//       queryFn: fetchTargetPlatformData,
+//     }),
+//   ]);
+
+//   return (
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       <ListGridWrapper />
+//     </HydrationBoundary>
+//   );
+// }

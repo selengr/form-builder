af29b1645ac22d779672cf4d81b7e70791f3a0f3
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-02-19 18:59:28 +0330

    Convert csr data fetching to SSR for builder -- part eleven - fix3

--- src/app/(builder)/user-reports/_hooks/usePostChangeStatus.ts
+++ src/app/(builder)/user-reports/_hooks/usePostChangeStatus.ts
@@ -1,29 +1,17 @@
 import { toast } from 'sonner';
-import { AxiosApi } from '@/services/axios/AxiosApi';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
+// actions
+import { postChangeStatusAction,  type TTicketFormData } from '../../../../../actions/userReports/postChangeStatusAction';
 
-interface TTicketFormData {
-  formId: string | string[];
-  ticket: string;
-  publicationApprovalByAdmin: boolean;
-}
-
-const postChangeStatus = async (data: TTicketFormData) => {
-  const url = `/admin/form/change-status`;
-  const response = await AxiosApi.put(url, data);
-  return response.data;
-};
+export const CHANGE_STATUS_MUTATION_KEY = ['change-status'] as const;
 
 export const usePostChangeStatus = () => {
-  const queryClient = useQueryClient();
 
   const mutation = useMutation({
-    mutationKey: ['change-status'],
-    mutationFn: ({ data }: { data: TTicketFormData }) => postChangeStatus(data),
+  mutationKey: CHANGE_STATUS_MUTATION_KEY,
+    mutationFn: ({ data }: { data: TTicketFormData }) => postChangeStatusAction(data),
 
     onSuccess: () => {
-      // queryClient.invalidateQueries([''] as any);
-      // queryClient.refetchQueries(['']as any);
       toast.success(`با موفقیت انجام شد`);
     },
     onError: () => {

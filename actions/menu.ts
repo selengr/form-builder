b2be98ef47dc1bcc85c7bc5112c9480e22a3958b
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-02-21 16:29:59 +0330

    change base url fix

--- actions/menu.ts
+++ actions/menu.ts
@@ -1,11 +1,13 @@
 'use server';
 
-import { serverApi } from '@/services/axios/serverApi';
+import { AxiosApi } from '@/services/axios/AxiosApi';
 import { IMenuResponseData } from '@/components/MiddleSidebar/type';
 
 export async function fetchMenuServer(): Promise<IMenuResponseData> {
   try {
-    const { data } = await serverApi.get('/authorization-psya/front-panel/non-org-user-role/find-user-loggedin-info');
+    const { data } = await AxiosApi.get('/authorization-psya/front-panel/non-org-user-role/find-user-loggedin-info', {
+      baseURL: process.env.BASE_URL,
+    });
     return data;
   } catch (err) {
     console.error('Fetch error:', err);

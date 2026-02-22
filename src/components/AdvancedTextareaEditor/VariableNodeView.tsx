3e3b2c4bf9779ee7b115a6089a1b44a26bf89b9e
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-02-22 20:45:44 +0330

    start implementing rich text editor in report - variableNodeView3

--- src/components/AdvancedTextareaEditor/VariableNodeView.tsx
+++ src/components/AdvancedTextareaEditor/VariableNodeView.tsx
@@ -1,10 +1,43 @@
-// test
-import React from 'react'
+"use client";
 
-export default function VariableNodeView() {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { useEffect, useRef, useState } from "react";
+import { NodeViewWrapper } from "@tiptap/react";
+import styles from "./advancedTextareaEditor.module.css";
+
+type Item = { unique_name: string; caption: string };
+
+type Props = {
+    node: any;
+    updateAttributes: (attrs: Record<string, any>) => void;
+    editor: any;
+    selected: boolean;
+    extension: any;
+};
+
+export default function VariableNodeView({ node, updateAttributes,
+    editor, selected, extension }: Props) {
+    const items: Item[] = (extension?.options?.items ?? []) as Item[];
+
+    const [open, setOpen] = useState(false);
+    const rootRef = useRef<HTMLSpanElement>(null);
+
+    const label = (node.attrs.label as string) || "";
+    const unique_name = (node.attrs.unique_name as string) || "";
+    const hasValue = Boolean(unique_name);
+
+    useEffect(() => {
+        const onDocDown = (e: MouseEvent) => {
+            if (!rootRef.current) return;
+            if (!rootRef.current.contains(e.target as Node)) setOpen(false);
+        };
+        document.addEventListener("mousedown", onDocDown);
+        return () => document.removeEventListener("mousedown", onDocDown);
+    }, []);
+
+
+    return (
+        <NodeViewWrapper as="span" data-variable="true" contentEditable={false}>
+           
+        </NodeViewWrapper >
+    );
 }

afb280184f56a6aaa0816ad64fea7b31a6de8c89
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-02-22 20:36:34 +0330

    start implementing rich text editor in report - variable

--- src/components/AdvancedTextareaEditor/Variable.ts
+++ src/components/AdvancedTextareaEditor/Variable.ts
@@ -1,1 +1,46 @@
-//test
+
+import VariableNodeView from "./VariableNodeView";
+import { Node, mergeAttributes } from "@tiptap/core";
+import { ReactNodeViewRenderer } from "@tiptap/react";
+
+type Item = { unique_name: string; caption: string };
+
+export const Variable = Node.create({
+    name: "variable",
+
+    group: "inline",
+    inline: true,
+    atom: true,
+    selectable: true,
+
+    addOptions() {
+        return {
+            items: [] as Item[],
+        };
+    },
+
+
+    addNodeView() {
+        return ReactNodeViewRenderer(VariableNodeView);
+    },
+
+    addCommands() {
+        return {
+            insertVariable:
+                () =>
+                    ({ commands }) =>
+                        commands.insertContent({
+                            type: this.name,
+                            attrs: { unique_name: "", label: "" },
+                        }),
+        };
+    },
+});
+
+declare module "@tiptap/core" {
+    interface Commands<ReturnType> {
+        variable: {
+            insertVariable: () => ReturnType;
+        };
+    }
+}

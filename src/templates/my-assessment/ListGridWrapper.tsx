5de18bd3b490a267cfe19d3ca8a4d9b816f05838
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-02-24 14:29:05 +0330

    adding my assessment filters

--- src/templates/my-assessment/ListGridWrapper.tsx
+++ src/templates/my-assessment/ListGridWrapper.tsx
@@ -39,6 +39,14 @@ export default function ListGridWrapper() {
     });
   };
 
+    const handleReportChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormType((prev: any) => ({ ...prev, showReport: (event.target as HTMLInputElement).value}));
+  };
+
+  const handleDoneStatusChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormType((prev: any) => ({ ...prev, takeParts: (event.target as HTMLInputElement).value}));
+  };
+
   return (
     <ListGrid
       title='ارزیابی‌های من'
@@ -46,14 +54,14 @@ export default function ListGridWrapper() {
       filterBoxList={filterBoxList}
       url='/user/form/main-list'
       filterComponent={
-        <div className='flex h-full w-full flex-col items-center justify-between'>
+        <div className='flex h-[calc(100vh-60px)] w-full flex-col items-center justify-between'>
           <div className='w-full h-[52px] flex items-center justify-center gap-4 rounded-lg bg-[#F7F7FF] px-2 mb-4'>
             <div className='flex items-center w-full justify-center gap-2'>
               <Image src={FilterIcon} width={30} height={30} alt='filter' />
               <p className='text-[16px] text-center font-bold text-[#161616]'>فیلتر</p>
             </div>
           </div>
-          <div className='flex flex-col gap-4 w-full'>
+          <div className='flex flex-col gap-4 w-full overflow-y-scroll h-full'>
             <div className='flex flex-col gap-4'>
               <div className='w-full flex flex-col justify-center gap-4 rounded-[20px] bg-[#F7F7FF] px-4 pt-4 pb-3'>
                 <FormControl
@@ -112,6 +120,55 @@ export default function ListGridWrapper() {
                     <FormControlLabel value='ALL' control={<Radio />} label='همه' />
                     <FormControlLabel value='PUBLIC' control={<Radio />} label='عمومی' />
                     <FormControlLabel value='PRIVATE' control={<Radio />} label='خصوصی' />
+                  </RadioGroup>
+                </FormControl>
+              </div>
+
+
+              <div className='w-full flex flex-col justify-center gap-4 rounded-[20px] bg-[#F7F7FF] px-4 pt-4 pb-3'>
+                <FormControl
+                  sx={{
+                    '& .MuiTypography-root': { fontSize: '14px', color: '#393939', fontWeight: 400 },
+                  }}>
+                  <FormLabel
+                    sx={{
+                      fontSize: '15px',
+                      color: '#161616',
+                      fontWeight: 700,
+                      mb: '8px',
+                      '&.Mui-focused': { color: '#161616' },
+                    }}>
+                    بر اساس گزارش
+                  </FormLabel>
+
+                  <RadioGroup value={formType.report} onChange={handleReportChange}>
+                    <FormControlLabel value='ALL' control={<Radio />} label='همه' />
+                    <FormControlLabel value='show' control={<Radio />} label='دارای گزارش' />
+                    <FormControlLabel value='not_show' control={<Radio />} label='بدون گزارش' />
+                  </RadioGroup>
+                </FormControl>
+              </div>
+
+              <div className='w-full flex flex-col justify-center gap-4 rounded-[20px] bg-[#F7F7FF] px-4 pt-4 pb-3'>
+                <FormControl
+                  sx={{
+                    '& .MuiTypography-root': { fontSize: '14px', color: '#393939', fontWeight: 400 },
+                  }}>
+                  <FormLabel
+                    sx={{
+                      fontSize: '15px',
+                      color: '#161616',
+                      fontWeight: 700,
+                      mb: '8px',
+                      '&.Mui-focused': { color: '#161616' },
+                    }}>
+                    بر اساس وضعیت
+                  </FormLabel>
+
+                  <RadioGroup value={formType.doneStatus} onChange={handleDoneStatusChange}>
+                    <FormControlLabel value='ALL' control={<Radio />} label='همه' />
+                    <FormControlLabel value='only_answered' control={<Radio />} label='انجام شده' />
+                    <FormControlLabel value='not_answered' control={<Radio />} label='انجام نشده' />
                   </RadioGroup>
                 </FormControl>
               </div>

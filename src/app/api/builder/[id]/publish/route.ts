ff21e741a8fc2d1f2ad9ee56e65f637ff83e9a95
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-03-29 16:16:48 +0330

    completed creating data-collection page and menu - fix api

--- src/app/api/builder/[id]/publish/route.ts
+++ src/app/api/builder/[id]/publish/route.ts
@@ -8,15 +8,23 @@ export const revalidate = 0;
 
 const publishSchema = z.object({
   IsSurvey: z.boolean(),
+  IsDataCollection: z.boolean(),
 });
 
 export async function PUT(req: Request, { params }: { params: { id: string } }) {
   const formId = params.id
 
   const body = await req.json()
-  const { IsSurvey } = publishSchema.parse(body)
-
-  const endpoint = IsSurvey ? `/admin/form/survey/finalization/${formId}` : `/form/ready-to-publish/${formId}`
+  const { IsSurvey, IsDataCollection } = publishSchema.parse(body)
+  let url = ''
+  if(IsSurvey){
+    url = `/admin/form/survey/finalization/${formId}`
+  }else if(IsDataCollection){
+    url = `/admin/form/data-colleection/finalization/${formId}`
+  } else {
+     url = `/form/ready-to-publish/${formId}`
+  }
+  const endpoint = url
 
   const newReq = new Request(req.url, {
     method: "PUT",

275bc452ab3ff312e42998fbc1eda4489a1d427b
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-03-29 19:13:39 +0330

    completed creating data-collection page and menu - fix targetPlatformEnum

--- actions/dataCollection/list.ts
+++ actions/dataCollection/list.ts
@@ -10,7 +10,7 @@ interface SearchBoxItem {
 }
 
 const PAGE_SIZE = 10;
-const DEFAULT_SEARCH_FILTER = { surveyTargetPlatformEnum: 'ALL', fieldOperation : "DSC"  };
+const DEFAULT_SEARCH_FILTER = { targetPlatformEnum: 'ALL', fieldOperation : "DSC"  };
 
 export async function dataCollectionFilter(
   pageParam: number,
@@ -22,11 +22,11 @@ export async function dataCollectionFilter(
   try {
     const filterRestrictions: SearchBoxItem[] = [];
 
-    if (searchQueryFilter.surveyTargetPlatformEnum && searchQueryFilter.surveyTargetPlatformEnum !== 'ALL') {
+    if (searchQueryFilter.targetPlatformEnum && searchQueryFilter.targetPlatformEnum !== 'ALL') {
       filterRestrictions.push({
-        fieldName: 'formSetting.surveyTargetPlatformEnum',
+        fieldName: 'formSetting.targetPlatformEnum',
         fieldOperation: 'EQUAL',
-        fieldValue: searchQueryFilter.surveyTargetPlatformEnum,
+        fieldValue: searchQueryFilter.targetPlatformEnum,
         nextConditionOperator: 'AND',
       });
     }

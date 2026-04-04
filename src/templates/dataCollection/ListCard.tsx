679a3251d718adca8ed3cc9829601802345db80e
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-04-04 19:01:07 +0330

    fix data collaction list - completed

--- src/templates/dataCollection/ListCard.tsx
+++ src/templates/dataCollection/ListCard.tsx
@@ -14,7 +14,8 @@ import { ISurveyItem } from '@/types/survey';
 import CopyIcon from '@/../public/images/home-page/copy.svg';
 import EditIcon from '@/../public/images/home-page/edit-2.svg';
 import TrashIcon from '@/../public/images/home-page/trash.svg';
-import CheckIcon from '@/../public/images/home-page/check.svg';
+import { CodiconEye } from '../../../public/images/home-page/EyeIcon';
+// import CodiconEye from '@/../public/images/home-page/CodiconEye';
 // import ShareLinkModal from './ShareLinkModal';
 
 interface ListCardProps {
@@ -112,7 +113,8 @@ const ListCard: React.FC<ListCardProps> = ({
           {data.status === 'PUBLISH' && (
             <div onClick={handleNavigation}>
               <IconButton disabled={loading} color='primary'>
-                <Image src={CheckIcon} alt='edit' width={24} height={24} />
+                {/* <Image src={CheckIcon} alt='edit' width={24} height={24} /> */}
+                <CodiconEye />
               </IconButton>
             </div>
           )}

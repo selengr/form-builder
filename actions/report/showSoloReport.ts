5e817cbbbb56c6ba5d3a2bb8f87c762c197860b9
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-04-09 23:44:43 +0330

    resolve, check and test exceptions in answer question -- showSoloReport

--- actions/report/showSoloReport.ts
+++ actions/report/showSoloReport.ts
@@ -5,7 +5,18 @@ import { serverApi } from '@/services/axios/serverApi';
 export async function showSoloReport(
   data: { formId: number; takePartId: number }[]
 ) {
-  const url = '/report/solo/show-solo-report';
-  const response = await serverApi.post(url, data);
-  return response.data;
+  try {
+    const url = '/report/solo/show-solo-report';
+    const response = await serverApi.post(url, data);
+    return response.data;
+  } catch (error: any) {
+    const message =
+      error?.response?.data?.message?.[0]?.title ||
+      error?.response?.data?.message ||
+      error?.response?.data ||
+      error?.message ||
+      'خطای نامشخص';
+
+    throw new Error(message);
+  }
 }

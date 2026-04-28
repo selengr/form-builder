484743a6cbcbc05b16be7eb25dd94a73e4b7592f
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-04-28 14:41:27 +0330

    fix menu loading

--- src/components/MiddleSidebar/menuItemSkeleton/index.tsx
+++ src/components/MiddleSidebar/menuItemSkeleton/index.tsx
@@ -1,8 +1,8 @@
-const MenuItemSkeleton = () => {
+const MenuItemSkeleton = ({ className = "" }) => {
   return (
     <>
       {[...Array(1)].map((_, index) => (
-        <div key={index} className='flex items-center gap-3 animate-pulse w-full my-3'>
+        <div key={index} className={`flex items-center gap-3 animate-pulse w-full my-3 ${className}`}>
           <div className='h-6 w-6 bg-gray-200 rounded-full'></div>
           <div className='h-5 w-[80%] bg-gray-200 rounded'></div>
         </div>

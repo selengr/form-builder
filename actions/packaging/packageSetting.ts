e6a65ba8a071ce374a19fbcb37404609cd45302f
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-04 02:48:44 +0330

    finish the whole process of changing new business

--- actions/packaging/packageSetting.ts
+++ actions/packaging/packageSetting.ts
@@ -3,9 +3,9 @@
 import { serverApi } from '@/services/axios/serverApi';
 
 export async function getPackageSettingAction(id: number) {
-  try{  
+  try {
     const url = `/admin/packaging/${id}`
-  
+
     const { data } = await serverApi.get(url);
     return data;
   } catch (error: any) {
@@ -13,7 +13,7 @@ export async function getPackageSettingAction(id: number) {
       error?.response?.data?.message?.[0]?.title ||
       error?.response?.data?.message ||
       'انجام عملیات با خطا مواجه شد'
-  
+
     throw new Error(message);
   }
 }
@@ -26,14 +26,29 @@ export async function putPackageSettingAction(
   try {
     const url = `/admin/packaging/${id}`;
     const response = await serverApi.put(url, payload);
-  
+
     return response?.data;
   } catch (error: any) {
     const message =
       error?.response?.data?.message?.[0]?.title ||
       error?.response?.data?.message ||
       'انجام عملیات با خطا مواجه شد'
-  
+
+    throw new Error(message);
+  }
+}
+
+export async function updatePackagingValidity(id: number, invalid: boolean) {
+  try {
+    const url = `/admin/packaging/main-list/invalid`;
+    const response = await serverApi.put(url, { id, invalid });
+
+    return response?.data;
+  } catch (error: any) {
+    const message =
+      error?.response?.data?.message?.[0]?.title ||
+      'خطا در تغییر وضعیت بسته'
+
     throw new Error(message);
   }
 }

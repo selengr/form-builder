6490c8b3136b05074a80f5ab2b3449b0ccfb198c
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-08 17:32:44 +0330

    created package purpose action

---
+++ actions/packaging/getPackagePurposeAction.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { serverApi } from '@/services/axios/serverApi';
+
+export interface IGetPackagePurpose {
+  value: string;
+  caption: string;
+}
+
+type IPurposeResponse = {
+  dataList: IGetPackagePurpose[];
+};
+
+export async function getPackagePurposeAction(): Promise<IPurposeResponse> {
+try{  
+  const customComboFilterModel = {
+    type: 'COMBO',
+    entity: 'PROJECTS',
+    input: '',
+    page: 0,
+    rows: 1000,
+  };
+
+  const baseUrl = `/admin/form/packaging/packaging-purpose/custom-combo`;
+  const queryString = `?customComboFilterModel=${encodeURIComponent(JSON.stringify(customComboFilterModel))}`;
+  const url = baseUrl + queryString;
+
+  const { data } = await serverApi.get<IPurposeResponse>(url);
+  return data;
+} catch (error: any) {
+  const message =
+    error?.response?.data?.message?.[0]?.title ||
+    error?.response?.data?.message ||
+    error?.response?.data ||
+    error?.message ||
+    'خطای نامشخص';
+
+  throw new Error(message);
+}
+}

60c216c9935c19e10ee84163b04914f5448d3a64
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-18 22:45:16 +0330

    fix: prevent double API fetch and search reset in ListGrid infinite query - update v10

--- src/components/SettingsDialog/SettingsDialog.tsx
+++ src/components/SettingsDialog/SettingsDialog.tsx
@@ -22,6 +22,7 @@ import FormProvider, { RHFTextField } from '../hook-form';
 
 // actions
 import { formSetting } from '../../../actions/builder/form-setting';
+import { useQueryClient } from '@tanstack/react-query';
 
 interface Props {
   data: any;
@@ -106,6 +107,7 @@ export default function SettingsDialog({
 }: Props) {
   const { id: formId } = useParams();
   const searchParams = useSearchParams();
+      const queryClient = useQueryClient()
 
   const [openDialog, setOpenDialog] = useState(false);
 
@@ -149,7 +151,7 @@ export default function SettingsDialog({
       await formSetting(isBuilderCardId ?? (formId as string), body);
 
       toast.success('تنظیمات با موفقیت ثبت شد');
-
+        queryClient.invalidateQueries({ queryKey: ["datas_builder_query"] })
       onChangeName?.(values.name);
 
       onChangeLimitation?.(

e0defa710df53cb4ccb5ed78ba71e1f828b1c0ae
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-22 00:28:38 +0330

    change contact-us pagee from csr to force-static - fix

--- src/templates/header/HeaderWrapper.tsx
+++ src/templates/header/HeaderWrapper.tsx
@@ -2,6 +2,7 @@
 
 import clsx from "clsx";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Logo from "../../../public/images/logo/logo2.svg";
 import MobileMenu from "@/components/MiddleSidebar/mobile/MobileMenu";
@@ -26,14 +27,17 @@ export default function LayoutWrapper({
       {!isSurvey && (
         <div className="bg-white w-full mx-auto px-4 py-3 flex justify-between items-center md:hidden fixed top-0 left-0 right-0 z-50 shadow-sm">
           <MobileMenu />
-          <Image
-            src={Logo}
-            alt="سایا لوگو"
-            width={120}
-            height={40}
-            priority
-            draggable={false}
-          />
+
+          <Link href="/" aria-label="بازگشت به خانه" className="cursor-pointer">
+            <Image
+              src={Logo}
+              alt="سایا لوگو"
+              width={120}
+              height={40}
+              priority
+              draggable={false}
+            />
+          </Link>
         </div>
       )}
 

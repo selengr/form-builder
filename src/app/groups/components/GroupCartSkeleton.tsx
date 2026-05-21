6b54ebdc4aca3d3c92d398312e666c3ce0c34caa
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-21 21:54:53 +0330

    updated my groups server action - GroupCartSkeleton

---
+++ src/app/groups/components/GroupCartSkeleton.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+const GroupCartSkeleton = () => {
+  return (
+    <div className="w-full max-w-lg bg-white rounded-xl border p-4 animate-pulse flex items-center justify-between">
+      
+      <div className="flex flex-col gap-2 flex-1">
+        <div className="h-4 w-40 bg-gray-200 rounded"></div>
+        <div className="h-3 w-24 bg-gray-200 rounded"></div>
+      </div>
+
+      <div className="h-6 w-10 bg-gray-200 rounded-full"></div>
+      
+    </div>
+  );
+};
+
+export default GroupCartSkeleton;

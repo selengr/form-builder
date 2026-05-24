025d4fe33698bf04a3a0bc6854f587720fdb97d8
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-24 22:28:42 +0330

    start the process of updaing form builder(/id)page - part five change csr to ssr

--- src/app/(builder)/builder/[id]/condition/_hooks/useGetList.ts
+++ src/app/(builder)/builder/[id]/condition/_hooks/useGetList.ts
@@ -1,17 +1,17 @@
-import { useParams } from 'next/navigation';
-import { useQuery } from '@tanstack/react-query';
-// action
-import { getConditionListAction } from '../../../../../../../actions/condition/getConditionListAction';
+// import { useParams } from 'next/navigation';
+// import { useQuery } from '@tanstack/react-query';
+// // action
+// import { getConditionListAction } from '../../../../../../../actions/condition/getConditionListAction';
 
-export const useGetList = () => {
-  const { id } = useParams();
-  return useQuery({
-    queryKey: ['Condition_List', id],
-    queryFn: () => getConditionListAction(String(id)),
-    staleTime: 0,
-    gcTime: 600000,
-    refetchOnWindowFocus: true,
-    refetchOnReconnect: true,
-    retry: 3,
-  });
-};
+// export const useGetList = () => {
+//   const { id } = useParams();
+//   return useQuery({
+//     queryKey: ['Condition_List', id],
+//     queryFn: () => getConditionListAction(String(id)),
+//     staleTime: 0,
+//     gcTime: 600000,
+//     refetchOnWindowFocus: true,
+//     refetchOnReconnect: true,
+//     retry: 3,
+//   });
+// };

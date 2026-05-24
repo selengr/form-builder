e573e41a263db9de0fc0fcf032d61b4fb71d6fbe
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-24 21:07:00 +0330

    start the process of updaing form builder(/id)page - calculator page  increase speed by 50%

--- src/app/(builder)/builder/[id]/calculator/page.tsx
+++ src/app/(builder)/builder/[id]/calculator/page.tsx
@@ -1,16 +1,6 @@
 'use client';
 import CalculatorList from '@/templates/calculator/CalculatorList';
 import { getCalculationListAction } from '../../../../../../actions/calculator/calculation';
-// import ClientView from './ClientView';
-// _hooks
-// import { useGetList } from './_hooks';
-
-// export default function CalculatorPage() {
-//   const { data, isPending, error } = useGetList();
-//   return <ClientView calculators={data} isPending={isPending} error={error} />;
-// }
-
-
 
 
 export default async function CalculatorPage({ params }: { params: Promise<{ id: string }> }) {
@@ -21,10 +11,6 @@ export default async function CalculatorPage({ params }: { params: Promise<{ id:
    const data = await getCalculationListAction(id)
 
   return (
-    <div className='w-full h-screen px-4 py-4'>
-      <div className='md:container mx-auto flex pb-3 flex-col  min-w-screen h-full justify-start items-center bg-white rounded-xl w-full'>
         <CalculatorList calculators={data} />
-    </div>
-    </div>
   )
 }

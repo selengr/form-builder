18b63db8eefe2224f704106c311e1bad53f45742
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-24 21:10:42 +0330

    start the process of updaing form builder(/id)page - part three

--- src/app/(builder)/builder/[id]/calculator/loading.tsx
+++ src/app/(builder)/builder/[id]/calculator/loading.tsx
@@ -1,12 +1,7 @@
-// app/calculator/[id]/loading.tsx
-
 import { CalculatorSkeleton } from "@/templates/calculator";
 
 export default function Loading() {
   return (
-    <div style={{ padding: 16 }}>
-      <p>در حال بارگذاری...</p>
        <CalculatorSkeleton />
-    </div>
   );
 }
--- src/app/(builder)/builder/[id]/condition/loading.tsx
+++ src/app/(builder)/builder/[id]/condition/loading.tsx
@@ -1,12 +1,7 @@
-// app/calculator/[id]/loading.tsx
-
 import { ConditionSkeleton } from "@/templates/condition";
 
 export default function Loading() {
   return (
-    <div style={{ padding: 16 }}>
-      <h1>در حال بارگذاری...</h1>
        <ConditionSkeleton />
-    </div>
   );
 }

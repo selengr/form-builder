17a81fc23c4e3204bf3fbf7ca640a0fa5eba1d74
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-24 21:20:46 +0330

    start the process of updaing form builder(/id)page - part three - fix

--- src/app/(builder)/builder/[id]/calculator/page.tsx
+++ src/app/(builder)/builder/[id]/calculator/page.tsx
@@ -1,16 +1,26 @@
-'use client';
 import CalculatorList from '@/templates/calculator/CalculatorList';
 import { getCalculationListAction } from '../../../../../../actions/calculator/calculation';
 
 
 export default async function CalculatorPage({ params }: { params: Promise<{ id: string }> }) {
-   const { id } = await params;
+  const { id } = await params;
 
-    if (!id) return <div>آیدی نامعتبر است</div>;
+  if (!id) return <div>آیدی نامعتبر است</div>;
 
-   const data = await getCalculationListAction(id)
+  const data = await getCalculationListAction(id)
 
   return (
-        <CalculatorList calculators={data} />
+    <CalculatorList calculators={data} />
   )
 }
+
+
+// 'use client';
+// import ClientView from './ClientView';
+// // _hooks
+// import { useGetList } from './_hooks';
+
+// export default function CalculatorPage() {
+//   const { data, isPending, error } = useGetList();
+//   return <ClientView calculators={data} isPending={isPending} error={error} />;
+// }

a1ea5767c822a8f2e80ab795bab9f2d7ac0a2940
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-26 17:59:43 +0330

    bring server action get to ssr file direct calling - safer & faster

--- src/app/(builder)/builder/[id]/page.tsx
+++ src/app/(builder)/builder/[id]/page.tsx
@@ -1,5 +1,15 @@
 import FormBuilderWapper from '@/templates/builder/FormBuilderWapper';
-import { getFormDataAction } from '../../../../../actions/builder/getForm';
+import { serverApi } from '@/services/axios/serverApi';
+// import { getFormDataAction } from '../../../../../actions/builder/getForm';
+
+
+
+
+export async function getFormDataAction(id: string) {
+    // await new Promise((resolve)=> setTimeout(() => resolve, 500))
+    const response = await serverApi.get(`/form/${id}`);
+    return response.data;
+}
 
 export default async function BuilderPage({ params }: { params: { id: string } }) {
  const data = await getFormDataAction(params.id)

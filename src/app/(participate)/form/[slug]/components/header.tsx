8e343c87baf6364c199bfbc33fe0b0e0a33ee2b4
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-27 22:11:51 +0330

    start responsiving participate - part two

--- src/app/(participate)/form/[slug]/components/header.tsx
+++ src/app/(participate)/form/[slug]/components/header.tsx
@@ -1,8 +1,8 @@
+import React from 'react';
+import Image from 'next/image';
 import { Button, IconButton } from '@mui/material';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
-import Image from 'next/image';
 import BugIcon from '@/../public/images/home-page/menu/bugIcon.svg';
-import React from 'react';
 
 interface HeaderProps {
   handleOpenReportDialog: any;
@@ -11,19 +11,37 @@ interface HeaderProps {
   surveyParam: boolean;
 }
 
-export function Header({ handleOpenReportDialog, replace, formName, surveyParam }: HeaderProps) {
+function Header({ handleOpenReportDialog, replace, formName, surveyParam }: HeaderProps) {
   return (
-    <div className='shrink-0 flex items-center justify-center gap-4 bg-[#F7F7FF] rounded-lg px-4 py-4 mb-4 relative m-2 z-10'>
-      {
-        !surveyParam && <IconButton sx={{ position: 'absolute', left: '8px' }} onClick={() => replace('/')}>
-          <MdOutlineKeyboardArrowRight color='#292D32' />
-        </IconButton> 
-      }
-      <p className='text-base font-bold text-[#161616] text-center mx-7'>{formName}</p>
+    <div className="shrink-0 relative m-2 mb-4 rounded-lg bg-[#F7F7FF] px-4 py-4 z-10">
 
-      <Button onClick={handleOpenReportDialog} size='medium' className='rounded-full' sx={{ position: 'absolute', right: '8px' }} endIcon={<Image alt='report' src={BugIcon} height={24} width={24} />}>
-        <span className='text-xs'>گزارش</span>
+      {!surveyParam && (
+        <IconButton
+          sx={{ position: 'absolute', left: '8px', top: '50%', transform: 'translateY(-50%)' }}
+          onClick={() => replace('/')}
+        >
+          <MdOutlineKeyboardArrowRight color="#292D32" />
+        </IconButton>
+      )}
+
+      <Button
+        onClick={handleOpenReportDialog}
+        size="medium"
+        className="rounded-full"
+        sx={{ position: 'absolute', right: '8px', top: '50%', transform: 'translateY(-50%)' }}
+        endIcon={<Image alt="report" src={BugIcon} height={24} width={24} />}
+      >
+        <span className="text-xs">گزارش</span>
       </Button>
+
+      <div className={`${surveyParam ? "px-0" : "px-12"} mx-5 flex items-center justify-center`}>
+        <p
+          className="text-base font-bold text-[#161616] text-center truncate max-w-full"
+          title={formName}
+        >
+          {formName}
+        </p>
+      </div>
     </div>
   );
 }

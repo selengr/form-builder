9cec3a4c5e4d7023bfcb4eedb91778f11445f766
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-05-28 19:54:22 +0330

    start preview responsiveness - part two skeleton - fix3

--- src/components/Fields/MultipleChoiceField.tsx
+++ src/components/Fields/MultipleChoiceField.tsx
@@ -272,6 +272,7 @@ function FormComponent({ elementInstance, onChange, error, value }: { elementIns
           marginBottom: description ? '0.5rem' : '2rem',
           fontWeight: '600',
           color: '#353535',
+          fontSize: { xs: 15, sm: 16 },
           '&.MuiFormLabel-root.MuiFormLabel-colorPrimary.Mui-focused': {
             color: '#353535',
           },
@@ -279,11 +280,7 @@ function FormComponent({ elementInstance, onChange, error, value }: { elementIns
         id={String(element?.questionId)}>
         {element.title}
       </FormLabel>
-      {description && (
-        <Typography sx={{ fontSize: '12px', fontWeight: '500', marginBottom: '2rem' }} variant='subtitle2'>
-          {description}
-        </Typography>
-      )}
+
       {isMultipleChoiceSelectionAllowed ? (
         <>
           <FormGroup>
@@ -302,6 +299,11 @@ function FormComponent({ elementInstance, onChange, error, value }: { elementIns
           </RadioGroup>
           {!!error && <Typography color='#f44336'>{error}</Typography>}
         </>
+      )}
+      {description && (
+        <Typography sx={{ fontSize: '12px', fontWeight: '500', marginTop: '2rem' }} variant='subtitle2'>
+          {description}
+        </Typography>
       )}
     </FormControl>
   );
@@ -440,22 +442,22 @@ function PropertiesComponent({ elementInstance }: { elementInstance: FormElement
       delete removeId.questionId;
 
       try {
-          const { data }: any = await createQuestionAction(removeId as any);
+        const { data }: any = await createQuestionAction(removeId as any);
         delete data.questionPropertyList;
         delete data.optionList;
         delete data.spectralPlaceList;
         const newData = {
           ...data,
         };
-debugger
+        debugger
         const positionToUse = lastIndexOfGroup === -1 ? firstIndexAfterThePreviousSelectedGroup : lastIndexOfGroup + 1;
         addElement(selectedElement?.position?.realPosition ?? positionToUse, newData);
 
         setOpenDialog(false);
         setSelectedElement(null);
         reset();
-      } catch (error:any) {
-         toast.error( error?.message || 'انجام عملیات با خطا مواجه شد');
+      } catch (error: any) {
+        toast.error(error?.message || 'انجام عملیات با خطا مواجه شد');
       }
     } else {
       try {
@@ -470,8 +472,8 @@ debugger
         setOpenDialog(false);
         setSelectedElement(null);
         reset();
-      } catch (error:any) {
-         toast.error( error?.message || 'انجام عملیات با خطا مواجه شد');
+      } catch (error: any) {
+        toast.error(error?.message || 'انجام عملیات با خطا مواجه شد');
       }
     }
   }
@@ -510,7 +512,7 @@ debugger
                   borderRadius: '10px',
                 },
               }}>
-              <RHFTextField name='label' dir='ltr'/>
+              <RHFTextField name='label' dir='ltr' />
             </Box>
           </Stack>
         }

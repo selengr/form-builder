a3a4fb7eb287a525d9008c28af409d073b4d8fbb
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-06-01 21:33:08 +0330

    updated survey

--- actions/survey/surveyFilter.ts
+++ actions/survey/surveyFilter.ts
@@ -19,7 +19,7 @@ export async function surveyFilter(
   url: string,
   searchQueryFilter = DEFAULT_SEARCH_FILTER,
 ) {
-  // try {
+  try {
     const filterRestrictions: SearchBoxItem[] = [];
 
     if (searchQueryFilter.isCreatedSoloReport && searchQueryFilter.isCreatedSoloReport !== 'ALL') {
@@ -70,21 +70,16 @@ console.log('validCombinedRestrictionList--------------------------------------'
 
     const response = await serverApi.get(fullURL);
 
-    // if (!response?.data) {
-    //   throw new Error('خطا در دریافت اطلاعات');
-    // }
-
     return {
+      success: true,
       data: response.data.content,
       total: response.data.totalElements,
     };
-    
-  // } catch (error: any) {
-  //   const message =
-  //     error?.response?.data?.message?.[0]?.title ||
-  //     error?.response?.data?.message ||
-  //     'انجام عملیات با خطا مواجه شد';
 
-  //   throw new Error(error);
-  // }
+    } catch (error: any) {
+        return {
+        success: false,
+        message: error?.message
+      };
+   }
 }

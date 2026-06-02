e86683b34ea3190a8c767cd141222e7c79e198f2
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-06-02 18:38:50 +0330

    update createQuestionAction api in multi choice

--- src/components/Fields/MultipleChoiceField.tsx
+++ src/components/Fields/MultipleChoiceField.tsx
@@ -443,14 +443,19 @@ function PropertiesComponent({ elementInstance }: { elementInstance: FormElement
       delete removeId.questionId;
 
       try {
-        const { data }: any = await createQuestionAction(removeId as any);
+        const res = await createQuestionAction(removeId as any);
+
+        if (!res.success) {
+          toast.error(res.message || 'انجام عملیات با خطا مواجه شد');
+          return;
+        }
+        const data = res.data
         delete data.questionPropertyList;
         delete data.optionList;
         delete data.spectralPlaceList;
         const newData = {
           ...data,
         };
-        debugger
         const positionToUse = lastIndexOfGroup === -1 ? firstIndexAfterThePreviousSelectedGroup : lastIndexOfGroup + 1;
         addElement(selectedElement?.position?.realPosition ?? positionToUse, newData);
 
@@ -462,7 +467,12 @@ function PropertiesComponent({ elementInstance }: { elementInstance: FormElement
       }
     } else {
       try {
-        const { data }: any = await updateQuestionAction(String(finalFieldData?.questionId), finalFieldData);
+        const res: any = await updateQuestionAction(String(finalFieldData?.questionId), finalFieldData);
+        if (!res.success) {
+          toast.error(res.message || 'انجام عملیات با خطا مواجه شد');
+          return;
+        }
+        const data = res.data
         delete data.questionPropertyList;
         delete data.optionList;
         delete data.spectralPlaceList;

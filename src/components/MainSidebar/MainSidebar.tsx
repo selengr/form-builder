51b7a50c30d4161dc6541d430dc39c6628b9c49a
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-06-03 20:52:33 +0330

    use UserInfoProvider usage -   clearUserInfo after logout بهط

--- src/components/MainSidebar/MainSidebar.tsx
+++ src/components/MainSidebar/MainSidebar.tsx
@@ -9,13 +9,12 @@ import { signIn, signOut } from 'next-auth/react';
 import Avatar from '@/components/Avatar/Avatar';
 import MenuSidebar from '@/components/SideBar/MenuSidebar';
 import MiddleSidebar from '../MiddleSidebar/MiddleSidebar';
-// hooks
-// import { useUserInfoNew } from '@/hooks/useUserInfoNew';
+// context
+import { useUserInfo } from '@/context/UserInfoContext ';
 // images
 import LogoutIcon from '@/../public/images/home-page/logout.svg';
 import InfoIcon from '@/../public/images/home-page/info-icon.svg';
 import MresalatLogo from '@/../public/images/home-page/mresalat_logo.svg';
-import { useUserInfo } from '@/context/UserInfoContext ';
 
 const SidebarButton = ({ icon, label, onClick }: { icon: JSX.Element; label: string; onClick?: () => void }) => (
   <button onClick={onClick} className='flex flex-col items-center gap-1 min-h-[40px]'>
@@ -27,9 +26,8 @@ const SidebarButton = ({ icon, label, onClick }: { icon: JSX.Element; label: str
 export default function MainSidebar() {
   const pathname = usePathname();
   const isSurvey = pathname.includes('survey-');
-  // const { userInfo, isAuthenticated, loading } = useUserInfoNew();
-    const { userInfo, isAuthenticated, clearUserInfo } = useUserInfo();
-  const loading = false; 
+  const { userInfo, isAuthenticated, clearUserInfo } = useUserInfo();
+  const loading = false;
   const avatar = userInfo?.user?.fullName;
   const endPoint = process.env.NEXT_PUBLIC_MRESALAT_ENDPOINT + '/';
 

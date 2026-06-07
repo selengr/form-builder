19cf94f46963e5cf77ed81501a3c12e44f021ed9
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-06-07 18:10:49 +0330

    update handlePreview in packaging

--- src/templates/packaging/ListCard.tsx
+++ src/templates/packaging/ListCard.tsx
@@ -74,8 +74,20 @@ const ListCard: React.FC<ListCardProps> = ({
     router.push(`/builder/${data.formId}?admin=packaging&pid=${data.id}`);
   };
 
+  // const handlePreview = () => {
+  //   router.push(`/preview/${data.formId}?rep=list`);
+  // };
+
   const handlePreview = () => {
-    router.push(`/preview/${data.formId}?rep=list`);
+    if (!data.formId) return;
+    const params = new URLSearchParams({
+      from: 'TESTING',
+    });
+    if (data.packagingStausEnum === 'CREATE') {
+      router.push(`/preview/${data.formId}?rep=list`)
+    } else {
+      router.push(`form/${data.formId}?${params.toString()}`);
+    }
   };
 
   const handleNavigateToReport = () => {
@@ -106,7 +118,7 @@ const ListCard: React.FC<ListCardProps> = ({
         <div className="flex items-center gap-1 flex-wrap">
           <div className="max-w-[100px] md:min-w-[105px]">
             <ActionButton
-              label="مشاهده"
+              label={data.packagingStausEnum === 'CREATE' ? "مشاهده" : "پیش نمایش"}
               onClick={handlePreview}
               color="#1758BA"
               hoverColor="#216ee1"

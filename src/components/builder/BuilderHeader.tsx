8510f36b0b7037e98e3921f20a5508b789e0e6b5
selengr <reza1997karbakhsh@gmail.com>
2026-07-31 17:18:10 +0330

    Updating the builder header preview link

--- src/components/builder/BuilderHeader.tsx
+++ src/components/builder/BuilderHeader.tsx
@@ -48,7 +48,7 @@ export default function BuilderHeader({
   const actionButtons = (
     <div className="flex items-center gap-2">
      
-        <Link href={`/preview/${id}`}>
+        <Link href={`/form/${id}?from=TESTING`}>
         <IconButton
           sx={{
             height: 32,

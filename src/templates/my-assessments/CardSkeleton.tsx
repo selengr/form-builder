ee075baeb3707fb88111421ea76a71c273bc4698
selengr <reza1997karbakhsh@gmail.com>
2026-09-14 20:08:35 +0330

    /my-assessments is on UnifiedListGrid with updated actions

--- src/templates/my-assessments/CardSkeleton.tsx
+++ src/templates/my-assessments/CardSkeleton.tsx
@@ -6,29 +6,26 @@ export default function CardSkeleton() {
   return (
     <>
       {Array.from({ length: 5 }).map((_, i) => (
-        <Grid2
-          key={i}
-          sx={{ width: 1, mx: 'auto', maxWidth: '470px' }}
-          size={{ xs: 12, md: 10, xl: 9 }}>
+        <Grid2 key={i} sx={{ width: '100%', mx: 'auto' }} size={12}>
           <div className="border border-[#DDE1E6] p-3 rounded-2xl flex flex-col gap-0 w-full max-w-full">
             <div className="space-y-2 mt-2">
               <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Skeleton variant="text" width="30%" />
-                <Skeleton variant="text" width="50%" />
+                <Skeleton variant="text" width="30%" animation="wave" />
+                <Skeleton variant="text" width="50%" animation="wave" />
               </Box>
               <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Skeleton variant="text" width="25%" />
-                <Skeleton variant="text" width="40%" />
+                <Skeleton variant="text" width="25%" animation="wave" />
+                <Skeleton variant="text" width="40%" animation="wave" />
               </Box>
               <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Skeleton variant="text" width="35%" />
-                <Skeleton variant="text" width="45%" />
+                <Skeleton variant="text" width="35%" animation="wave" />
+                <Skeleton variant="text" width="45%" animation="wave" />
               </Box>
             </div>
 
             <div className="flex w-full gap-4 flex-col sm:flex-row mt-2">
-              <Skeleton variant="rounded" height={42} sx={{ flex: 1 }} />
-              <Skeleton variant="rounded" height={42} sx={{ flex: 1 }} />
+              <Skeleton variant="rounded" height={42} sx={{ flex: 1 }} animation="wave" />
+              <Skeleton variant="rounded" height={42} sx={{ flex: 1 }} animation="wave" />
             </div>
           </div>
         </Grid2>

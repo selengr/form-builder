fc81d46ce033b3c438e53609889b9ddb307f3343
selengr <reza1997karbakhsh@gmail.com>
2026-09-19 22:54:57 +0330

    update all cart

--- actions/participate/showResultParticipate.ts
+++ actions/participate/showResultParticipate.ts
@@ -1,22 +1,10 @@
 'use server';
 
-import { serverApi } from '@/services/axios/serverApi';
+import { api } from '@/services/axios/actionWapper';
 
-export async function showResultParticipate(
-    data: { formId: number; takePartId: number }
-) {
-    // try {
-        const url = '/report/user/solo/show-solo-report';
-        const response = await serverApi.post(url, data);
-        return response.data;
-    // } catch (error: any) {
-    //     const message =
-    //         error?.response?.data?.message?.[0]?.title ||
-    //         error?.response?.data?.message ||
-    //         error?.response?.data ||
-    //         error?.message ||
-    //         'خطای نامشخص';
-
-    //     throw new Error(message);
-    // }
+export async function showResultParticipate(data: {
+  formId: number;
+  takePartId: number;
+}) {
+  return api.post('/report/user/solo/show-solo-report', data);
 }
--- src/app/(participate)/form/[slug]/show-result/hooks/useShowResultUser.tsx
+++ src/app/(participate)/form/[slug]/show-result/hooks/useShowResultUser.tsx
@@ -1,22 +1,32 @@
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
-// action
-import { showResultParticipate } from '../../../../../../../actions/participate/showResultParticipate';
+import { showResultParticipate } from '@actions/participate/showResultParticipate';
 
 export const useShowResultUser = () => {
   const { push } = useRouter();
 
   const mutation = useMutation({
     mutationKey: ['Show_Solo_Result'],
-    mutationFn: ({ data }: { data: { formId: number; takePartId: number }; name: string }) => showResultParticipate(data),
+    mutationFn: async ({
+      data,
+    }: {
+      data: { formId: number; takePartId: number };
+      name: string;
+    }) => {
+      const res = await showResultParticipate(data);
+      if (!res.success) {
+        throw new Error(res.message || 'انجام عملیات با خطا مواجه شد');
+      }
+      return res.data;
+    },
 
     onSuccess: (result, { name }) => {
       localStorage.setItem('Show_Solo_Result', JSON.stringify(result));
       push(`/form/${result?.formId}/show-result?name=${name}`);
     },
     onError: (error) => {
-      toast.error( error?.message || 'انجام عملیات با خطا مواجه شد');
+      toast.error(error?.message || 'انجام عملیات با خطا مواجه شد');
     },
   });
 

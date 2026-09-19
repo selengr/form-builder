6490ffb12958ea803fe9b3098610742b4ba0410d
selengr <reza1997karbakhsh@gmail.com>
2026-09-19 23:04:04 +0330

    update auth

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { serverApi } from '@/services/axios/serverApi';
+import { api } from '@/services/axios/actionWapper';
 
 export interface IUser {
   id: number;
@@ -17,29 +17,29 @@ export interface IUserInfoResponse {
   userRoles: any[];
 }
 
-export interface IUserInfo   {
+export interface IUserInfo {
   userInfo: IUserInfoResponse | null;
   isAuthenticated: boolean;
   error: string | Error | null;
 }
 
-export async function fetchUserInfoServer(): Promise<IUserInfo  > {
-  try {
-    const response = await serverApi.get<any>('/authorization/front-panel/non-org-user-role/find-user-loggedin-info',{
-       baseURL: process.env.BASE_URL,
-    });
+export async function fetchUserInfoServer(): Promise<IUserInfo> {
+  const res = await api.get<IUserInfoResponse>(
+    '/authorization/front-panel/non-org-user-role/find-user-loggedin-info',
+    { baseURL: process.env.BASE_URL },
+  );
 
-    return {
-      userInfo: response.data,
-      isAuthenticated: true,
-      error: null,
-    };
-  } catch (error: any) {
-    const err = error instanceof Error ? error : new Error(error?.message || 'خطای نامشخص');
+  if (!res.success) {
     return {
       userInfo: null,
       isAuthenticated: false,
-      error: err,
+      error: new Error(res.message || 'خطای نامشخص'),
     };
   }
+
+  return {
+    userInfo: res.data,
+    isAuthenticated: true,
+    error: null,
+  };
 }

f0de97106b94e842bd81b131d42ed2914781d94c
Reza Karbakhsh 1640 <ui4@fardup.co>
2026-09-21 17:33:41 +0330

    up

--- src/app/(participate)/form/[slug]/components/ErrorStep.tsx
+++ src/app/(participate)/form/[slug]/components/ErrorStep.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import React from 'react';
 import { Button } from '@mui/material';
 import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
@@ -21,16 +20,30 @@ export function ErrorStep({ message, replace }: ErrorStepProps) {
   );
 
   return (
-    <div className='w-full flex flex-col p-4 overflow-hidden'>
+    <div className='w-full h-full flex flex-col p-4 overflow-hidden'>
       <div className='flex flex-col bg-white rounded-xl h-full max-h-screen'>
         <div className='flex-1 flex items-center justify-center overflow-y-auto px-4'>
           <div className='w-full max-w-3xl'>
             <AnimatedBox>
               <div className='w-full flex flex-col items-center justify-center gap-4 text-center'>
-                <p className='text-xl font-bold text-red-400 mb-4'>{message}</p>
+                <p className='text-xl font-bold text-red-400 mb-2'>{message}</p>
+                {message.includes("شما قبلا به این فرم پاسخ داده اید") && (
+                  <p className='text-lg mb-0'>
+                     امکان پاسخ‌دهی مجدد توسط سازنده فرم محدود شده است. 
+                     <p className='text-lg'>
+                     از مشارکت شما سپاسگزاریم.
+                     </p>
+                  </p>
+                )}
+                {message.includes("شما قبلا به صورت کامل به این فرم پاسخ داده اید") && (
+                  <p className='text-lg mb-0'>
+                     امکان پاسخ‌دهی مجدد توسط سازنده فرم محدود شده است. از مشارکت شما سپاسگزاریم.
+
+                  </p>
+                )}
 
                 <div className='w-full max-w-xs sm:max-w-md'>
-                  <Image src={errorStep} alt='خطا' width={400} height={400} priority className='w-full h-auto max-h-[400px] object-contain' draggable={false} unoptimized/>
+                  <Image src={errorStep} alt='خطا' width={400} height={400} priority className='w-full h-auto max-h-[400px] object-contain' draggable={false} unoptimized />
                 </div>
 
                 <Button
